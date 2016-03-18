b200cf580edc4c62b59e93f6a8bc5e746d9c5c0f
Fnoop <fnoopdogg@gmail.com>
2016-03-18 17:02:29 +0000

    Fixes #1: Install puppet if not installed, only works on debian to start with

--- runme.sh
+++ runme.sh
@@ -6,4 +6,22 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Check that puppet is installed
+if ! hash puppet 2>/dev/null; then
+	echo 'Puppet not installed, attempting to install..'
+	if hash apt-get 2>/dev/null; then
+		DEBIAN_FRONTEND=noninteractive apt-get -y install puppet >/dev/null 2>&1
+		if hash puppet; then
+			puppetinstalled=true
+		fi
+	fi
+else
+	puppetinstalled=true
+fi
+if ! $puppetinstalled; then
+	echo 'Error: Puppet not installed and could not be installed'
+	exit 1
+fi
+
+# OK we're good to go!
 puppet apply --confdir=conf --environment $1 manifests
