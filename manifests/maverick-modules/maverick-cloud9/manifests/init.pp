3cdc168954d27eaad9e2160a1d0a04ca6099f993
Fnoop <fnoopdogg@gmail.com>
2016-03-18 21:11:27 +0000

    Fix cloud9 install

--- manifests/maverick-modules/maverick-cloud9/manifests/init.pp
+++ manifests/maverick-modules/maverick-cloud9/manifests/init.pp
@@ -4,14 +4,18 @@ class maverick-cloud9 (
     ) {
     file { "/srv/maverick/software/cloud9":
         ensure 		=> directory,
-        require		=> File["/srv/maverick/software"]
-
+        require		=> File["/srv/maverick/software"],
+	mode		=> 755,
+	owner		=> "mav",
+	group		=> "mav",
     } ->
     vcsrepo { "/srv/maverick/software/cloud9":
         ensure		=> present,
         provider 	=> git,
         source		=> "https://github.com/c9/core.git",
         revision	=> "master",
+	owner		=> "mav",
+	group		=> "mav",
         notify		=> [ Exec["install-cloud9"], Exec["install-cloud9-warning"] ]
     } ->
     exec { "install-cloud9-warning":
@@ -21,10 +25,11 @@ class maverick-cloud9 (
     } ->
     exec { "install-cloud9":
         command		=> "/srv/maverick/software/cloud9/scripts/install-sdk.sh",
-        cwd		    => "/srv/maverick/software/cloud9",
+        cwd		=> "/srv/maverick/software/cloud9",
         timeout		=> 0,
-        refreshonly	=> true,
-        user        => "mav"
+        #refreshonly	=> true,
+        user        	=> "mav",
+	environment 	=> ["HOME=/home/mav"],
     } ->
     file { "/etc/systemd/system/cloud9.service":
         content     => template("maverick-cloud9/cloud9.service.erb"),
