d2798e92c29e06e03481c73e91050b914e9b5c46
fnoop <fnoopdogg@gmail.com>
2016-03-22 00:46:59 +0000

    #42: Disable can bus by blacklisting kernel moddules

--- manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
+++ manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
@@ -1,6 +1,12 @@
 class maverick-baremetal::beagle::init (
     ) {
     
+    file { "/etc/modprobe.d/can-blacklist.conf":
+        content     => template("maverick-baremetal/can-blacklist.conf.erb")
+        mode        => 644,
+        owner       => "root",
+        group       => "root",
+    }
     # Disable can devices as they seem to flood and lockup the kernel
     #service { "sys-subsystem-net-devices-can0.device":
     #    ensure      => stopped,
