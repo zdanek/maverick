5be0bcf9632ce39c2920d185080a026fd28b33f9
fnoop <fnoopdogg@gmail.com>
2016-03-22 00:48:14 +0000

    #42: Disable can bus by blacklisting kernel moddules

--- manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
+++ manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
@@ -2,7 +2,7 @@ class maverick-baremetal::beagle::init (
     ) {
     
     file { "/etc/modprobe.d/can-blacklist.conf":
-        content     => template("maverick-baremetal/can-blacklist.conf.erb")
+        content     => template("maverick-baremetal/can-blacklist.conf.erb"),
         mode        => 644,
         owner       => "root",
         group       => "root",
