aa0e0c9fd640888f6137c7ed9b08743a5d05c924
fnoop <fnoopdogg@gmail.com>
2016-03-22 07:54:30 +0000

    Remove security and networking from base class

--- manifests/maverick-modules/base/manifests/init.pp
+++ manifests/maverick-modules/base/manifests/init.pp
@@ -66,12 +66,6 @@ class base {
         class { "maverick-baremetal": }
     }
     
-    ### Setup security
-    # class { "maverick-security": }
-    
-    ### Setup networking
-    class { "maverick-network": }
-
     ### NOTE: Hiera will also include classes, depending on hierarchy.
     ### In particular, check hiera/defaults.json which includes classes that are run on every server,
     ###  and $environment/defaults.json, which contain default classes/values for that environment.
