830a5c6a08f5ab951e2d9c8ad57b7fedc6f23969
fnoop <fnoopdogg@gmail.com>
2016-03-22 00:58:52 +0000

    Tidy up beagle init.pp

--- manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
+++ manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
@@ -7,23 +7,6 @@ class maverick-baremetal::beagle::init (
         owner       => "root",
         group       => "root",
     }
-    # Disable can devices as they seem to flood and lockup the kernel
-    #service { "sys-subsystem-net-devices-can0.device":
-    #    ensure      => stopped,
-    #    enable      => false,
-    #} ->
-    #service { "sys-devices-platform-ocp-481cc000.can-net-can0.device":
-    #    ensure      => stopped,
-    #    enable      => false,
-    #}
-    #service { "sys-subsystem-net-devices-can1.device":
-    #    ensure      => stopped,
-    #    enable      => false,
-    #} ->
-    #service { "sys-devices-platform-ocp-481d0000.can-net-can1.device":
-    #    ensure      => stopped,
-    #    enable      => false,
-    #}
-    
+
     
 }
