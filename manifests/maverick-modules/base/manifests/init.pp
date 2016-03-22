a4d2131e84396c0e7bb66c12851877458f9bb047
fnoop <fnoopdogg@gmail.com>
2016-03-22 20:23:04 +0000

    Add networking back into base class

--- manifests/maverick-modules/base/manifests/init.pp
+++ manifests/maverick-modules/base/manifests/init.pp
@@ -66,6 +66,9 @@ class base {
         class { "maverick-baremetal": }
     }
     
+    ## Network setup
+    class { "maverick-network": }
+    
     ### NOTE: Hiera will also include classes, depending on hierarchy.
     ### In particular, check hiera/defaults.json which includes classes that are run on every server,
     ###  and $environment/defaults.json, which contain default classes/values for that environment.
