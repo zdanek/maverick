2d7dbbb7107a4c3c8cd384d202f13f7fa7f71e72
fnoop <fnoopdogg@gmail.com>
2016-03-25 09:26:08 +0000

    Fixes #57 odroid hostip fix, fix defaults

--- manifests/maverick-modules/base/manifests/hostip.pp
+++ manifests/maverick-modules/base/manifests/hostip.pp
@@ -1,8 +1,14 @@
 class base::hostip {
-    # If we have debian loopaddress set, make sure it's set to current hostname
-    exec { "loophost":
-        unless      => "/bin/grep -E '127.0.1.1\\s+${hostname}' /etc/hosts",
+    # If we have debian 127.0.1.1 loopaddress set, make sure it's set to current hostname
+    exec { "loophost11":
+        onlyif      => "/bin/grep '127.0.1.1' /etc/hosts |/bin/grep -vE '127.0.1.1\\s+${hostname}'",
         command     => "/bin/sed /etc/hosts -i -r -e 's/127.0.1.1\\s+(.*)/127.0.1.1\\t${hostname}/'"
+    }
+
+    # If we have debian 127.0.0.1 loopaddress set, make sure it's set to current hostname but ignore localhost
+    exec { "loophost01":
+        onlyif      => "/bin/grep '127.0.0.1' /etc/hosts |/bin/grep -v localhost |/bin/grep -vE '127.0.0.1\\s+${hostname}'",
+        command     => "/bin/sed /etc/hosts -i -r -e 's/127.0.0.1\\s+(.*)/127.0.0.1\\t${hostname}/'"
     }
 
     # Retrieve host/ip values from hiera
