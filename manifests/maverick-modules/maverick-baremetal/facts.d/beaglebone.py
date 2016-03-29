e5b890905a921a9b15089e75a83633576579bc54
Maverick user <mav@beaglebone>
2016-03-29 17:15:06 +0000

    Fixes #38: Expand beaglebone rootfs

--- manifests/maverick-modules/maverick-baremetal/facts.d/beaglebone.py
+++ manifests/maverick-modules/maverick-baremetal/facts.d/beaglebone.py
@@ -7,32 +7,32 @@ from xml.dom.minidom import parse, parseString
 
 # Check if model path available, if not exit
 if not os.path.isfile('/sys/firmware/devicetree/base/model'):
-	print "beagle_present=no"
-	sys.exit(1)
+    print "beagle_present=no"
+    sys.exit(1)
 
 # Define main data container
 data = {'emmcbooted': 'no', 'sdcard_present': 'no'}
 
 f = open('/sys/firmware/devicetree/base/model', 'r')
 for line in f:
-	if re.search('BeagleBone Black', line):
-		data['present'] = 'yes'
-		data['model'] = 'BeagleBone Black'
-	else:
-		print "beagle_present=no"
-		sys.exit(1)
+    if re.search('BeagleBone Black', line):
+        data['present'] = 'yes'
+        data['model'] = 'BeagleBone Black'
+    else:
+        print "beagle_present=no"
+        sys.exit(1)
 
 """
 try:
-	lshw_xml = commands.getoutput('/usr/bin/lshw -xml')
-	lshw_dom = parseString(lshw_xml)
-	nodes = lshw_dom = lshw_dom.childNodes
-	for node in nodes:
-		continue
-		#print node
-		#print '----'
+    lshw_xml = commands.getoutput('/usr/bin/lshw -xml')
+    lshw_dom = parseString(lshw_xml)
+    nodes = lshw_dom = lshw_dom.childNodes
+    for node in nodes:
+        continue
+        #print node
+        #print '----'
 except:
-	pass
+    pass
 """
 
 # Obtain the SD card size from proc
@@ -40,21 +40,31 @@ f = open('/proc/partitions', 'r')
 for line in f:
     if re.search("mmcblk0$", line):
         data['rootdisksize'] = int(line.split()[2]) / 1024
+    if re.search("mmcblk0p1$", line):
+        partsize = int(line.split()[2]) / 1024
+        try:
+            partpct = (data['rootdisksize'] / partsize) * 100
+            if partpct > 98:
+                data['rootdiskexpanded'] = 'yes'
+            else:
+                data['rootdiskexpanded'] = 'no'
+        except:
+            pass
     if re.search("mmcblk0boot0", line):
-    	data['emmcbooted'] = 'yes'
-    	data['emmc_size'] = data['rootdisksize']
+        data['emmcbooted'] = 'yes'
+        data['emmc_size'] = data['rootdisksize']
     if re.search("mmcblk1boot0", line):
-    	data['emmcbooted'] = 'no'
-    	data['sdcard_present'] = 'yes'
-    	data['sdcard_size'] = data['rootdisksize']
+        data['emmcbooted'] = 'no'
+        data['sdcard_present'] = 'yes'
+        data['sdcard_size'] = data['rootdisksize']
     try:
-    	if data['emmcbooted'] == 'yes' and re.search("mmcblk1$"):
-    		data['sdcard_present'] = 'yes'
-    		data['sdcard_size'] = int(line.split()[2]) / 1024
-    	if data['emmcbooted'] == 'no' and re.search("mmcblk1$"):
-    		data['emmc_size'] = int(line.split()[2]) / 1024
+        if data['emmcbooted'] == 'yes' and re.search("mmcblk1$", line):
+            data['sdcard_present'] = 'yes'
+            data['sdcard_size'] = int(line.split()[2]) / 1024
+        if data['emmcbooted'] == 'no' and re.search("mmcblk1$", line):
+            data['emmc_size'] = int(line.split()[2]) / 1024
     except:
-    	pass
+        pass
 
 f.close()
 
@@ -62,5 +72,5 @@ f.close()
 if data:
     for key,val in data.items():
         print "beagle_%s=%s" % (key, val)
-	
+    
 
--- manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
+++ manifests/maverick-modules/maverick-baremetal/manifests/beagle/init.pp
@@ -9,5 +9,10 @@ class maverick-baremetal::beagle::init (
     }
 
     class { "maverick-baremetal::beagle::services": }
+
+   # Remove unnecessary packages from beagle installs to save space - space is at a premium on emmc
+    package { ["aglfn", "alsa-utils", "ap-hotspot", "blt", "bmap-tools", "dmidecode", "libblas-common", "libblas3", "libbluray1", "libcdio-cdda1", "libcdio-paranoia1", "libcdio13", "libcups2", "libdc1394-22", "libdc1394-22-dev", "libdrm-exynos1", "libdrm-nouveau2", "libdrm-radeon1", "libdrm-tegra0", "libflac8", "libid3tag0", "libimobiledevice4", "libogg0", "libraw1394-11", "libraw1394-dev", "libraw1394-tools", "libsamplerate0", "libschroedinger", "libthai-data", "libthai0"]:
+        ensure      => purged
+    }
     
 }
