00ae43a080b5dfd5380efb2e184a0791b1297208
fnoop <fnoopdog@gmail.com>
2016-03-31 12:15:53 +0000

    Fixes #75, wrong path in dronekit facts

--- manifests/maverick-modules/maverick-dronekit/facts.d/ardupilot.py
+++ manifests/maverick-modules/maverick-dronekit/facts.d/ardupilot.py
@@ -2,22 +2,22 @@
 
 import os.path
 print "ardupilotfw_test=yes"
-if os.path.isfile("/srv/maverick/code/dronekit-dev/ardupilot/ArduCopter/ArduCopter.elf"):
+if os.path.isfile("/srv/maverick/code/dronekit-sitl/sitl-fw/ArduCopter/ArduCopter.elf"):
     print "ardupilotfw_arducopter=yes"
 else:
     print "ardupilotfw_arducopter=no"
 
-if os.path.isfile("/srv/maverick/code/dronekit-dev/ardupilot/ArduPlane/ArduPlane.elf"):
+if os.path.isfile("/srv/maverick/code/dronekit-sitl/sitl-fw/ArduPlane/ArduPlane.elf"):
     print "ardupilotfw_arduplane=yes"
 else:
     print "ardupilotfw_arduplane=no"
 
-if os.path.isfile("/srv/maverick/code/dronekit-dev/ardupilot/APMrover2/APMrover2.elf"):
+if os.path.isfile("/srv/maverick/code/dronekit-sitl/sitl-fw/APMrover2/APMrover2.elf"):
     print "ardupilotfw_apmrover2=yes"
 else:
     print "ardupilotfw_apmrover2=no"
 
-if os.path.isfile("/srv/maverick/code/dronekit-dev/ardupilot/AntennaTracker/AntennaTracker.elf"):
+if os.path.isfile("/srv/maverick/code/dronekit-sitl/sitl-fw/AntennaTracker/AntennaTracker.elf"):
     print "ardupilotfw_antennatracker=yes"
 else:
     print "ardupilotfw_antennatracker=no"
