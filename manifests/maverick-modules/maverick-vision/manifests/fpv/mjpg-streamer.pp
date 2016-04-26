289af6c77d01101c78f07ae6f92bc51c38a3c7d0
fnoop <fnoopdogg@gmail.com>
2016-04-26 22:06:47 +0000

    Fix compiling every run

---
+++ manifests/maverick-modules/maverick-vision/manifests/fpv/mjpg-streamer.pp
@@ -0,0 +1,30 @@
+class maverick-vision::fpv::mjpg-streamer {
+    
+    ensure_packages(["cmake", "libjpeg-turbo8-dev"])
+    
+    # Add ocam_viewer repo from git
+    file { "/srv/maverick/software/mjpg-streamer":
+        ensure 		=> directory,
+        require		=> File["/srv/maverick/software"],
+        mode		=> 755,
+        owner		=> "mav",
+        group		=> "mav",
+    } ->
+    vcsrepo { "/srv/maverick/software/mjpg-streamer":
+        ensure		=> present,
+        provider 	=> git,
+        source		=> "https://github.com/jacksonliam/mjpg-streamer.git",
+        revision	=> "master",
+        owner		=> "mav",
+        group		=> "mav",
+    } ->
+    exec { "mjpg-streamer-compile":
+        user        => "mav",
+        timeout     => 0,
+        require     => Package["cmake", "libjpeg-turbo8-dev"],
+        command     => "/usr/bin/make -j${::processorcount}",
+        cwd         => "/srv/maverick/software/mjpg-streamer/mjpg-streamer-experimental",
+        creates     => "/srv/maverick/software/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer",
+    }
+    
+}
