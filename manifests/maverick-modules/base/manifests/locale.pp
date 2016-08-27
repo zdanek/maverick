16b9a53916c75f54ebd94d051cf3cf901390f3c1
Fnoop <fnoopdogg@gmail.com>
2016-08-27 09:51:07 +0100

    Fixes #148: Set default lcale

--- manifests/maverick-modules/base/manifests/locale.pp
+++ manifests/maverick-modules/base/manifests/locale.pp
@@ -1,6 +1,15 @@
 class base::locale (
+    $locale = "en_GB.UTF-8",
     $timezone = "Europe/London",
 ) {
+    
+    # Set system locale
+    ensure_packages(["language-pack-en"])
+    class { "locales":
+        default_locale      => "${locale}",
+        locales             => [ "${locale}" ],
+        require             => Package["language-pack-en"],
+    }
     
     # Set the timezone using systemd
     exec { "tz-systemd":
