576ca506c525282a10adb3ae5555118220a2ed25
Fnoop <fnoopdogg@gmail.com>
2016-09-26 23:12:24 +0100

    Fix sysctl config

--- manifests/maverick-modules/base/manifests/sysctl.pp
+++ manifests/maverick-modules/base/manifests/sysctl.pp
@@ -5,13 +5,13 @@ class base::sysctl {
     # $name is provided by define invocation
     $key = $name
 
-     #augeas { "sysctl_conf/$key":
-       #context => "/files/etc/sysctl.conf",
-     #  onlyif  => "get /files/etc/sysctl.conf/$key != '$value'",
-     #  changes => "set /files/etc/sysctl.conf/$key '$value'",
-     #  notify  => Exec["sysctl"],
-     #}
-     
+    # Add key if it doesn't already exist
+    exec {"sysctl_conf_add_$key":
+        command     => "/bin/echo '$key=$value' >>/etc/sysctl.conf",
+        unless      => "/bin/grep -e '^$key\s*=' /etc/sysctl.conf",
+    }
+
+    # Alter key if already exists
     exec { "sysctl_conf_$key":
         command     => "/bin/sed /etc/sysctl.conf -i -r -e 's/^$key\s*=.*/$key=$value/'",
         unless      => "/bin/grep -e '^$key\s*=\s*$value' /etc/sysctl.conf"
