a17e32b058fbb06e05378822c816c436681b8387
Fnoop <fnoopdogg@gmail.com>
2016-10-20 21:42:29 +0100

    Remove rtspserver

--- manifests/maverick-modules/maverick_vision/manifests/init.pp
+++ manifests/maverick-modules/maverick_vision/manifests/init.pp
@@ -18,7 +18,7 @@ class maverick_vision (
     
     # Install gstreamer
     if $gstreamer_installtype == "native" {
-        ensure_packages(["libgstreamer1.0-0", "libgstreamer-plugins-base1.0-dev", "libgstreamer1.0-dev", "gstreamer1.0-alsa", "gstreamer1.0-plugins-base", "gstreamer1.0-plugins-good", "gstreamer1.0-plugins-bad", "gstreamer1.0-plugins-ugly", "libgstrtspserver-1.0-0", "gstreamer1.0-tools", "python-gst-1.0", "gir1.2-gstreamer-1.0", "gir1.2-gst-plugins-base-1.0", "gir1.2-clutter-gst-2.0", "gir1.2-gst-rtsp-server-1.0"])
+        ensure_packages(["libgstreamer1.0-0", "libgstreamer-plugins-base1.0-dev", "libgstreamer1.0-dev", "gstreamer1.0-alsa", "gstreamer1.0-plugins-base", "gstreamer1.0-plugins-good", "gstreamer1.0-plugins-bad", "gstreamer1.0-plugins-ugly", "gstreamer1.0-tools", "python-gst-1.0", "gir1.2-gstreamer-1.0", "gir1.2-gst-plugins-base-1.0", "gir1.2-clutter-gst-2.0"])
         if ($raspberry_present == "yes") {
     		ensure_packages(["gstreamer1.0-omx"])
     	}
