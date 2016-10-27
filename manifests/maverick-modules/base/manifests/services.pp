ac036eff610bbf320b20e3d85b6b566ba62cd7e7
fnoop <fnoopdogg@gmail.com>
2016-10-27 09:19:35 +0100

    Turn off dev services in flight mode, fixes #193

--- manifests/maverick-modules/base/manifests/services.pp
+++ manifests/maverick-modules/base/manifests/services.pp
@@ -9,6 +9,12 @@ class base::services {
     package { ["cups", "cups-filters", "cups-filters-core-drivers", "printer-driver-*", "cups-daemon"]:
         ensure      => purged,
     }
+    # Stop postfix, we don't need a mail service/relay for now
+    service { "postfix":
+        ensure      => stopped,
+        enable      => false,
+    }
+
 
     # Here is where we would enable any services by default
     # service { "example":
@@ -16,4 +22,22 @@ class base::services {
     #   enable      => true
     # }
         
+    # If in flight mode as opposed to dev mode, specifically disable dev mode.  We do it here because
+    #  if dev module isn't included it can't disable itself.
+    # If you include dev module in flight mode (which is never intended by design), you will probably get a class clash here.
+    if $environment == "flight" {
+        service { "maverick-cloud9":
+            ensure      => stopped,
+            enable      => false,
+        }
+        service { "maverick-dev-sitl":
+            ensure      => stopped,
+            enable      => false,
+        }
+        #service { "maverick-mavros-sitl":
+        #    ensure      => stopped,
+        #    enable      => false,
+        #}
+    }
+    
 }
