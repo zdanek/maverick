681ecc58ba3d50a876169cb1adecfdbdbb3a15d5
Fnoop <fnoopdogg@gmail.com>
2016-11-02 21:55:36 +0000

    Remove unnecessary fail2ban.bak puppet module

--- manifests/puppet-modules/fail2ban.bak/manifests/service.pp
+++
@@ -1,9 +0,0 @@
-class fail2ban::service {
-
-  service { 'fail2ban':
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-  }
-
-}
--- manifests/puppet-modules/fail2ban.bak/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'fail2ban' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('fail2ban') }
-  end
-end
