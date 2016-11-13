d9d97edf2e2ed6dea56e7ef6d50f4e07d1e1f250
fnoop <fnoopdogg@gmail.com>
2016-11-13 17:35:31 +0000

    Misc fixes for wifibroadcast

--- manifests/maverick-modules/maverick_network/manifests/wifibroadcast.pp
+++ manifests/maverick-modules/maverick_network/manifests/wifibroadcast.pp
@@ -22,7 +22,7 @@ class maverick_network::wifibroadcast (
     } ->
     exec { "setcaps-wtx":
         command     => "/sbin/setcap cap_net_raw,cap_net_admin=eip /srv/maverick/software/wifibroadcast/tx",
-        unless      => "/sbin/getcap /srv/maverick/software/wifibroadcast/tx",
+        unless      => "/sbin/getcap /srv/maverick/software/wifibroadcast/tx |/bin/grep cap_net_admin",
     } ->
     exec { "copy-patched-9271-firmware":
         command     => "/bin/cp -f /srv/maverick/software/wifibroadcast/patches/AR9271/firmware/htc_9271.fw /lib/firmware",
