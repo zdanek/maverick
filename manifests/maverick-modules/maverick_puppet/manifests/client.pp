967c7ea1121d69e5cc63b7938d8902f39c716fe7
fnoop <fnoopdogg@gmail.com>
2017-02-28 15:21:44 +0000

    Iniital work on puppetlabs repos, #267

--- manifests/maverick-modules/maverick_puppet/manifests/client.pp
+++ manifests/maverick-modules/maverick_puppet/manifests/client.pp
@@ -1,20 +1,37 @@
 class maverick_puppet::client (
+    $puppetlabs = false,
 ) {
 
-    package {["puppet", "facter", "hiera"]:
-        ensure	=> installed
+    # If we are on a recognised/named OS, install the latest Puppet PC1
+    if $::lsbdistcodename and $puppetlabs {
+        # dpkg/apt can't install from a remote URL, so we have to use an exec
+        exec { "install-puppetlabs-pc1":
+            cwd         => "/var/tmp",
+            command     => "/usr/bin/wget https://apt.puppetlabs.com/puppetlabs-release-pc1-${::lsbdistcodename}.deb; /usr/bin/dpkg --force-confold -i puppetlabs-release-pc1-${::lsbdistcodename}.deb; /usr/bin/apt update" ,
+            unless      => "/usr/bin/dpkg -s puppetlabs-release-pc1",
+        } ->
+        package { ["puppet", "puppet-common"]:
+            ensure      => purged
+        } ->
+        package { "puppet-agent":
+            ensure      => installed
+        }
+    } else {
+        ensure_packages(["puppet", "facter", "hiera"])
     }
+
+    # Ensure puppet services are stopped, maverick runs puppet entirely in oneshot/masterless mode
+	service {["puppet", "puppetmaster"]:
+        ensure		=> "stopped",
+		enable		=> false,
+	}
+	
     if ($operatingsystem == "Debian" or $operatingsystem == "Ubuntu") {
         package {["ruby-json"]:
             ensure	=> installed
         }
     }
 
-    service {["puppet"]:
-        ensure		=> "stopped",
-        enable		=> false,
-        require     => Package["puppet"],
-    }
     file { "/etc/puppet/puppet.conf":
         ensure      => file,
         content     => template("maverick_puppet/puppet-client.conf.erb"),
