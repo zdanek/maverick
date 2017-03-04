5f7d6f051f65766b9d8ecc68c83c07d03f851a74
fnoop <fnoopdogg@gmail.com>
2017-03-04 08:14:22 +0000

    Add realsense samples and realsense sdk, fixes #287

--- manifests/maverick-modules/maverick_baremetal/manifests/peripheral/realsense.pp
+++ manifests/maverick-modules/maverick_baremetal/manifests/peripheral/realsense.pp
@@ -1,7 +1,7 @@
 class maverick_baremetal::peripheral::realsense (
 ) {
 
-    ensure_packages(["libglfw3", "libglfw3-dev", "libusb-1.0-0", "libusb-1.0-0-dev", "pkg-config", "libssl-dev"])
+    ensure_packages(["libglfw3", "libglfw3-dev", "libusb-1.0-0", "libusb-1.0-0-dev", "pkg-config", "libssl-dev", "liblz4-dev", "liblog4cxx-dev"])
 
     # Clone source from github
     oncevcsrepo { "git-realsense-librealsense":
@@ -56,5 +56,55 @@ class maverick_baremetal::peripheral::realsense (
         command         => "/sbin/udevadm control --reload-rules && /sbin/udevadm trigger",
         refreshonly     => true
     }
-
+    
+    # Clone examples source from github
+    file { "/srv/maverick/code/realsense":
+        ensure          => directory,
+        owner           => mav,
+        group           => mav,
+        mode            => "755",
+    } ->
+    oncevcsrepo { "git-realsense-realsense_samples":
+        gitsource   => "https://github.com/IntelRealSense/realsense_samples.git",
+        dest        => "/srv/maverick/code/realsense/samples",
+    }
+    
+    # Clone realsense-sdk
+    oncevcsrepo { "git-realsense-realsense_sdk":
+        gitsource   => "https://github.com/IntelRealSense/realsense_sdk.git",
+        dest        => "/srv/maverick/var/build/realsense-sdk",
+    } ->
+    # Create build directory
+    file { "/srv/maverick/var/build/realsense-sdk/build":
+        ensure      => directory,
+        owner       => "mav",
+        group       => "mav",
+        mode        => 755,
+    } ->
+    exec { "realsense-sdk-prepbuild":
+        user        => "mav",
+        timeout     => 0,
+        environment => ["LD_LIBRARY_PATH=/srv/maverick/software/opencv/lib", "PATH=/srv/maverick/software/opencv/bin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/sbin", "CMAKE_PREFIX_PATH=/srv/maverick/software/opencv"],
+        command     => "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/realsense-sdk -DCMAKE_INSTALL_RPATH=/srv/maverick/software/realsense-sdk/lib:/srv/maverick/software/librealsense/lib ..",
+        cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+        creates     => "/srv/maverick/var/build/realsense-sdk/build/Makefile",
+        require     => [ File["/srv/maverick/var/build/realsense-sdk/build"], Package["liblz4-dev"], Package["liblog4cxx-dev"] ], # ensure we have all the dependencies satisfied
+    } ->
+    exec { "realsense-sdk-build":
+        user        => "mav",
+        timeout     => 0,
+        environment => ["CPLUS_INCLUDE_PATH=/srv/maverick/software/librealsense/include:/srv/maverick/software/opencv/include", "LIBRARY_PATH=/srv/maverick/software/librealsense/lib:/srv/maverick/software/opencv/lib"],
+        command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/realsense-sdk.build.out 2>&1",
+        cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+        # creates     => "/srv/maverick/var/build/realsense-sdk/build/utils/aruco_tracker",
+        require     => Exec["realsense-sdk-prepbuild"],
+    } ->
+    exec { "realsense-sdk-install":
+        user        => "mav",
+        timeout     => 0,
+        command     => "/usr/bin/make install >/srv/maverick/var/log/build/realsense-sdk.install.out 2>&1",
+        cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+        # creates     => "/srv/maverick/software/realsense-sdk/bin/aruco_tracker",
+    }
+    
 }
