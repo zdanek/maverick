73dea8882153daa96538fb7b8b897b0f3be0d834
fnoop <fnoopdogg@gmail.com>
2017-03-06 07:18:23 +0000

    vision_landing compile fixes

--- manifests/maverick-modules/maverick_dev/manifests/dronekit.pp
+++ manifests/maverick-modules/maverick_dev/manifests/dronekit.pp
@@ -21,9 +21,12 @@ class maverick_dev::dronekit (
     } ->
     # Compile vision_landing
     exec { "vision_landing-compile":
+        user        => mav,
+        environment => ["LD_LIBRARY_PATH=/srv/maverick/software/opencv/lib:/srv/maverick/software/aruco/lib", "CMAKE_PREFIX_PATH=/srv/maverick/software/opencv:/srv/maverick/software/aruco"],
         cwd         => "/srv/maverick/code/dronekit-apps/vision_landing/src",
         command     => "/usr/bin/cmake . && make && make install",
         creates     => "/srv/maverick/code/dronekit-apps/vision_landing/track_targets",
+        require     => File["/srv/maverick/var/build/.install_flag_opencv"],
     } ->
     # Install systemd manifest
     file { "/etc/systemd/system/vision_landing.service":
@@ -37,7 +40,7 @@ class maverick_dev::dronekit (
         service { "vision_landing":
             ensure  => running,
             enable  => true,
-            require => Exec["vision_landing"]
+            require => Exec["vision_landing-compile"]
         }
     } else {
         service { "vision_landing":
