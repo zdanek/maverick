3b84336bd2f8fc3b462a5c4e92414d78d14e14c6
fnoop <fnoopdogg@gmail.com>
2017-05-10 09:57:09 +0100

    More mavros startup fixes

--- manifests/maverick-modules/maverick_ros/files/mavros.sh
+++ manifests/maverick-modules/maverick_ros/files/mavros.sh
@@ -2,9 +2,10 @@
 
 source /srv/maverick/software/ros/current/setup.bash
 [ ! -r /srv/maverick/data/config/ros/mavros-$1.conf ] || . /srv/maverick/data/config/ros/mavros-$1.conf
+export ROS_LOG_DIR
 
 echo "Waiting a few seconds for rosmaster to fully launch"
-sleep 5
+sleep 10
 
 DT=$(date +"%Y-%m-%d-%H-%M")
-ROS_MASTER_URI="http://localhost:${ROS_PORT}" /srv/maverick/software/ros/current/bin/roslaunch mavros apm.launch fcu_url:=tcp://localhost:${MAVLINK_PORT} >/srv/maverick/var/log/mavros/$1/mavros.DT.log 2>&1
+ROS_MASTER_URI="http://localhost:${ROS_PORT}" /srv/maverick/software/ros/current/bin/roslaunch mavros apm.launch fcu_url:=tcp://localhost:${MAVLINK_PORT} >/srv/maverick/var/log/ros/$1/mavros.$DT.log 2>&1
