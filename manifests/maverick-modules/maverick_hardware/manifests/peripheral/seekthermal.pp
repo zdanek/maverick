fb4c32387fa5fe0b1277e0bb877b61b0cf336195
fnoop <fnoopdogg@gmail.com>
2017-05-21 07:21:44 +0100

    More seek fixes, use development branch of libseek-thermal, #432

--- manifests/maverick-modules/maverick_hardware/manifests/peripheral/seekthermal.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/peripheral/seekthermal.pp
@@ -1,63 +1,86 @@
 class maverick_hardware::peripheral::seekthermal (
 ) {
     
-    # Install libseek-thermal
-    ensure_packages(["libusb-1.0-0-dev"])
-    oncevcsrepo { "git-libseek-thermal":
-        gitsource   => "https://github.com/maartenvds/libseek-thermal.git",
-        dest        => "/srv/maverick/software/libseek-thermal",
-    } ->
-    exec { "libseek-thermal-compile":
-        user        => "mav",
-        timeout     => 0,
-        environment => ["CXXFLAGS=-I/srv/maverick/software/opencv/include", "LDFLAGS=-L/srv/maverick/software/opencv/lib -R/srv/maverick/software/opencv/lib", "PKG_CONFIG_PATH=/srv/maverick/software/opencv/lib/pkgconfig"],
-        command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/libseek-thermal.build.log 2>&1",
-        cwd         => "/srv/maverick/software/libseek-thermal",
-        creates     => "/srv/maverick/software/libseek-thermal/lib/libseek.a",
-        require     => [ Package["libusb-1.0-0-dev"], Class["maverick_vision::opencv"] ],
-    } ->
+    # Install udev rule so seek thermal can be used by mav user
     file { "/etc/udev/rules.d/95-seekthermal.rules":
         owner       => "root",
         group       => "root",
         mode        => "644",
-        content     => "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"289D\", ATTRS{idProduct}==\"${$seekthermal_modelid}\", MODE=\"0666\", GROUP=\"users\"\n",
+        content     => "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"289d\", ATTRS{idProduct}==\"${$seekthermal_modelid}\", MODE=\"0664\", OWNER=\"mav\", GROUP=\"mav\"\n",
+    }
+
+    # Install libseek-thermal
+    ensure_packages(["libusb-1.0-0-dev", "libboost-program-options-dev"])
+    if ! ("install_flag_libseek-thermal" in $installflags) {
+        oncevcsrepo { "git-libseek-thermal":
+            gitsource   => "https://github.com/maartenvds/libseek-thermal.git",
+            dest        => "/srv/maverick/var/build/libseek-thermal",
+            revision    => "development",
+        } ->
+        exec { "libseek-thermal-compile":
+            user        => "mav",
+            timeout     => 0,
+            environment => ["CXXFLAGS=-I/srv/maverick/software/opencv/include", "LDFLAGS=-L/srv/maverick/software/opencv/lib -R/srv/maverick/software/opencv/lib", "PKG_CONFIG_PATH=/srv/maverick/software/opencv/lib/pkgconfig"],
+            command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/libseek-thermal.build.log 2>&1",
+            cwd         => "/srv/maverick/var/build/libseek-thermal",
+            creates     => "/srv/maverick/var/build/libseek-thermal/lib/libseek.so",
+            require     => [ Package["libusb-1.0-0-dev"], Class["maverick_vision::opencv"] ],
+        } ->
+        exec { "libseek-thermal-install":
+            user        => "mav",
+            command     => "/usr/bin/make install PREFIX=/srv/maverick/software/libseek-thermal",
+            cwd         => "/srv/maverick/var/build/libseek-thermal",
+            creates     => "/srv/maverick/software/libseek-thermal/lib/libseek.so",
+        } ->
+        file { "/srv/maverick/var/build/.install_flag_libseek-thermal":
+            ensure      => present,
+            owner       => mav,
+            mode        => 644
+        }
     }
     
     # Install libseek
-    oncevcsrepo { "git-libseek":
-        gitsource   => "https://github.com/zougloub/libseek.git",
-        dest        => "/srv/maverick/var/build/libseek",
-    } ->
-    file { "/srv/maverick/var/build/libseek/waf":
-        ensure      => present,
-        source      => "puppet:///modules/maverick_hardware/seek-waf",
-        owner       => "mav",
-        group       => "mav",
-        mode        => "755",
-    } ->
-    file { "/srv/maverick/software/libseek":
-        ensure      => directory,
-        owner       => "mav",
-        group       => "mav",
-        mode        => "755",
-    } ->
-    exec { "prep-libseek":
-        command     => "/srv/maverick/var/build/libseek/waf configure --prefix /srv/maverick/software/libseek",
-        cwd         => "/srv/maverick/var/build/libseek",
-        creates     => "/srv/maverick/var/build/libseek/build/blah",
-        user        => "mav",
-    } ->
-    exec { "compile-libseek":
-        command     => "/srv/maverick/var/build/libseek/waf",
-        cwd         => "/srv/maverick/var/build/libseek",
-        creates     => "/srv/maverick/var/build/libseek/build/blah",
-        user        => "mav",
-    } ->
-    exec { "install-libseek":
-        command     => "/srv/maverick/var/build/libseek/waf install --destdir /",
-        cwd         => "/srv/maverick/var/build/libseek",
-        creates     => "/srv/maverick/var/build/libseek/build/blah",
-        user        => "mav",
+    if ! ("install_flag_libseek" in $installflags) {
+        oncevcsrepo { "git-libseek":
+            gitsource   => "https://github.com/zougloub/libseek.git",
+            dest        => "/srv/maverick/var/build/libseek",
+        } ->
+        file { "/srv/maverick/var/build/libseek/waf":
+            ensure      => present,
+            source      => "puppet:///modules/maverick_hardware/seek-waf",
+            owner       => "mav",
+            group       => "mav",
+            mode        => "755",
+        } ->
+        file { "/srv/maverick/software/libseek":
+            ensure      => directory,
+            owner       => "mav",
+            group       => "mav",
+            mode        => "755",
+        } ->
+        exec { "prep-libseek":
+            command     => "/srv/maverick/var/build/libseek/waf configure --prefix /srv/maverick/software/libseek",
+            cwd         => "/srv/maverick/var/build/libseek",
+            creates     => "/srv/maverick/var/build/libseek/build/config.log",
+            user        => "mav",
+        } ->
+        exec { "compile-libseek":
+            command     => "/srv/maverick/var/build/libseek/waf",
+            cwd         => "/srv/maverick/var/build/libseek",
+            creates     => "/srv/maverick/var/build/libseek/build/seek-test",
+            user        => "mav",
+        } ->
+        exec { "install-libseek":
+            command     => "/srv/maverick/var/build/libseek/waf install --destdir /",
+            cwd         => "/srv/maverick/var/build/libseek",
+            creates     => "/srv/maverick/software/libseek/lib/libseek.a",
+            user        => "mav",
+        } ->
+        file { "/srv/maverick/var/build/.install_flag_libseek":
+            ensure      => present,
+            owner       => "mav",
+            mode        => 644,
+        }
     }
 
 }
