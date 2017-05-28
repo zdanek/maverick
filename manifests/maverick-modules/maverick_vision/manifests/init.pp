c90a74e21afbac86fbc021d3ee1a14385328fc05
fnoop <fnoopdogg@gmail.com>
2017-05-28 13:25:23 +0100

    Enable vision_seek if seekthermal hardware enabled, #432

--- manifests/maverick-modules/maverick_vision/manifests/init.pp
+++ manifests/maverick-modules/maverick_vision/manifests/init.pp
@@ -49,7 +49,7 @@ class maverick_vision (
         class { "maverick_vision::camera_streaming_daemon": }
     }
 
-    if $seekthermal_present == "yes" or $vision_seek == true {
+    if $seekthermal_present == "yes" or getvar("maverick_hardware::seekthermal_install") == true or $vision_seek == true {
         class { "maverick_vision::vision_seek": }
     }
     
