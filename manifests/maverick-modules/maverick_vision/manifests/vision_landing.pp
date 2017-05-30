aea3fbc736bd392a4eb9c6ddec5c96cb7d4242b5
fnoop <fnoopdogg@gmail.com>
2017-05-30 08:31:47 +0100

    Fix missing quote in vision_landing

--- manifests/maverick-modules/maverick_vision/manifests/vision_landing.pp
+++ manifests/maverick-modules/maverick_vision/manifests/vision_landing.pp
@@ -18,7 +18,7 @@ class maverick_vision::vision_landing (
     # Compile vision_landing
     exec { "vision_landing-compile":
         user        => mav,
-        environment => ["CMAKE_PREFIX_PATH=/srv/maverick/software/opencv:/srv/maverick/software/aruco:/srv/maverick/software/librealsense],
+        environment => ["CMAKE_PREFIX_PATH=/srv/maverick/software/opencv:/srv/maverick/software/aruco:/srv/maverick/software/librealsense"],
         cwd         => "/srv/maverick/software/vision_landing/src",
         command     => "/usr/bin/cmake -DCMAKE_MODULE_PATH=/srv/maverick/software/opencv . && make && make install",
         creates     => "/srv/maverick/software/vision_landing/track_targets",
