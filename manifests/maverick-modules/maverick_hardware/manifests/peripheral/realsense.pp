525e7f65451a26062113e18529bdb3fb5ce54255
fnoop <fnoopdogg@gmail.com>
2017-06-01 10:16:54 +0100

    Add flag to turn off realsense_sdk compile

--- manifests/maverick-modules/maverick_hardware/manifests/peripheral/realsense.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/peripheral/realsense.pp
@@ -1,4 +1,5 @@
 class maverick_hardware::peripheral::realsense (
+    $realsensesdk = true,
 ) {
 
     ensure_packages(["libglfw3", "libglfw3-dev", "libusb-1.0-0", "libusb-1.0-0-dev", "pkg-config", "libssl-dev", "liblz4-dev", "liblog4cxx-dev"])
@@ -102,47 +103,48 @@ class maverick_hardware::peripheral::realsense (
     }
     
     if ! ("install_flag_realsense_sdk" in $installflags) {
-
-        # Clone realsense-sdk
-        oncevcsrepo { "git-realsense-realsense_sdk":
-            gitsource   => "https://github.com/IntelRealSense/realsense_sdk.git",
-            dest        => "/srv/maverick/var/build/realsense-sdk",
-        } ->
-        # Create build directory
-        file { "/srv/maverick/var/build/realsense-sdk/build":
-            ensure      => directory,
-            owner       => "mav",
-            group       => "mav",
-            mode        => 755,
-        } ->
-        exec { "realsense-sdk-prepbuild":
-            user        => "mav",
-            timeout     => 0,
-            environment => ["LD_LIBRARY_PATH=/srv/maverick/software/opencv/lib", "PATH=/srv/maverick/software/opencv/bin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/sbin", "CMAKE_PREFIX_PATH=/srv/maverick/software/opencv"],
-            command     => "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/realsense-sdk -DCMAKE_INSTALL_RPATH=/srv/maverick/software/realsense-sdk/lib:/srv/maverick/software/librealsense/lib ..",
-            cwd         => "/srv/maverick/var/build/realsense-sdk/build",
-            creates     => "/srv/maverick/var/build/realsense-sdk/build/Makefile",
-            require     => [ File["/srv/maverick/var/build/realsense-sdk/build"], Package["liblz4-dev"], Package["liblog4cxx-dev"] ], # ensure we have all the dependencies satisfied
-        } ->
-        exec { "realsense-sdk-build":
-            user        => "mav",
-            timeout     => 0,
-            environment => ["CPLUS_INCLUDE_PATH=/srv/maverick/software/librealsense/include:/srv/maverick/software/opencv/include", "LIBRARY_PATH=/srv/maverick/software/librealsense/lib:/srv/maverick/software/opencv/lib"],
-            command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/realsense-sdk.build.out 2>&1",
-            cwd         => "/srv/maverick/var/build/realsense-sdk/build",
-            creates     => "/srv/maverick/var/build/realsense-sdk/build/sdk/src/core/pipeline/librealsense_pipeline.so",
-            require     => Exec["realsense-sdk-prepbuild"],
-        } ->
-        exec { "realsense-sdk-install":
-            user        => "mav",
-            timeout     => 0,
-            command     => "/usr/bin/make install >/srv/maverick/var/log/build/realsense-sdk.install.out 2>&1",
-            cwd         => "/srv/maverick/var/build/realsense-sdk/build",
-            creates     => "/srv/maverick/software/realsense-sdk/bin/realsense_fps_counter_sample",
-        } ->
-        file { "/srv/maverick/var/build/.install_flag_realsense_sdk":
-            ensure      => file,
-            owner       => "mav",
+        if $realsensesdk == true {
+            # Clone realsense-sdk
+            oncevcsrepo { "git-realsense-realsense_sdk":
+                gitsource   => "https://github.com/IntelRealSense/realsense_sdk.git",
+                dest        => "/srv/maverick/var/build/realsense-sdk",
+            } ->
+            # Create build directory
+            file { "/srv/maverick/var/build/realsense-sdk/build":
+                ensure      => directory,
+                owner       => "mav",
+                group       => "mav",
+                mode        => 755,
+            } ->
+            exec { "realsense-sdk-prepbuild":
+                user        => "mav",
+                timeout     => 0,
+                environment => ["LD_LIBRARY_PATH=/srv/maverick/software/opencv/lib", "PATH=/srv/maverick/software/opencv/bin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/sbin", "CMAKE_PREFIX_PATH=/srv/maverick/software/opencv"],
+                command     => "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/realsense-sdk -DCMAKE_INSTALL_RPATH=/srv/maverick/software/realsense-sdk/lib:/srv/maverick/software/librealsense/lib ..",
+                cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+                creates     => "/srv/maverick/var/build/realsense-sdk/build/Makefile",
+                require     => [ File["/srv/maverick/var/build/realsense-sdk/build"], Package["liblz4-dev"], Package["liblog4cxx-dev"] ], # ensure we have all the dependencies satisfied
+            } ->
+            exec { "realsense-sdk-build":
+                user        => "mav",
+                timeout     => 0,
+                environment => ["CPLUS_INCLUDE_PATH=/srv/maverick/software/librealsense/include:/srv/maverick/software/opencv/include", "LIBRARY_PATH=/srv/maverick/software/librealsense/lib:/srv/maverick/software/opencv/lib"],
+                command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/realsense-sdk.build.out 2>&1",
+                cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+                creates     => "/srv/maverick/var/build/realsense-sdk/build/sdk/src/core/pipeline/librealsense_pipeline.so",
+                require     => Exec["realsense-sdk-prepbuild"],
+            } ->
+            exec { "realsense-sdk-install":
+                user        => "mav",
+                timeout     => 0,
+                command     => "/usr/bin/make install >/srv/maverick/var/log/build/realsense-sdk.install.out 2>&1",
+                cwd         => "/srv/maverick/var/build/realsense-sdk/build",
+                creates     => "/srv/maverick/software/realsense-sdk/bin/realsense_fps_counter_sample",
+            } ->
+            file { "/srv/maverick/var/build/.install_flag_realsense_sdk":
+                ensure      => file,
+                owner       => "mav",
+            }
         }
     } 
 }
