8598a15b0fae4d1da4944cd24b4078c7fd2f0072
fnoop <fnoopdogg@gmail.com>
2017-06-08 22:48:16 +0100

    Add git tcp port to firewall, #461

--- manifests/maverick-modules/maverick_security/manifests/firewall_common.pp
+++ manifests/maverick-modules/maverick_security/manifests/firewall_common.pp
@@ -6,5 +6,12 @@ class maverick_security::firewall_common {
         proto       => tcp,
         action      => accept,
     }
+    
+    ### Allow git traffic
+    maverick_security::firewall::firerule { "git":
+        ports       => [9148],
+        ips         => hiera("firewall_ips"),
+        proto       => "tcp", # allow both tcp and udp for rtsp and rtp
+    }
 
 }
