448e3399f619d6638e58babf2f5c8656ae584920
fnoop <fnoopdogg@gmail.com>
2017-06-12 16:20:44 +0100

    Install package if not already exists in install_python_modules atleast

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,7 +24,16 @@ define install_python_module ($ensure, $pkgname=$title, $virtualenv=undef, $time
         }
       }
       'atleast': {
-        if versioncmp($version, $module_version) > 0 {
+        if empty($module_version) {
+            notice("Installing pip: ${pkgname}")
+            python::pip { $title:
+                pkgname => "${pkgname}",
+                ensure => 'present',
+                virtualenv => $virtualenv,
+                owner => $owner,
+                timeout => $timeout
+            }
+        } else if versioncmp($version, $module_version) > 0 {
             notice("Upgrading Pip module: ${pkgname}, installed version ${module_version} is less than requested version ${version}")
             python::pip { $title:
                 pkgname => "${pkgname}",
