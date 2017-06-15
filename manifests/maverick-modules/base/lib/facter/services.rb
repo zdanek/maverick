386f025328d4bb0275b268382a20a8167038c311
fnoop <fnoopdogg@gmail.com>
2017-06-15 23:22:41 +0100

    Fix services fact

--- manifests/maverick-modules/base/lib/facter/services.rb
+++ manifests/maverick-modules/base/lib/facter/services.rb
@@ -1,7 +1,7 @@
 Facter.add(:installed_services) do
   setcode do
     services = []
-    systemctl_output = Facter::Core::Execution::exec("systemctl -a -t service list-units |awk {'print $1'} |grep service |sed 's/\.service//'")
+    systemctl_output = Facter::Core::Execution::exec("systemctl --plain -a -t service list-units |awk {'print $1'} |grep service |sed 's/\.service//'")
     unless systemctl_output.to_s.empty?
         _services = systemctl_output.lines.map(&:chomp)
         _services.each do |_service|
