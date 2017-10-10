1a9a469550faaf5abcb0ed9a2a20d6d1307486cb
fnoop <fnoopdogg@gmail.com>
2017-10-10 14:33:11 +0100

    Update grafana to 4.5.2

--- manifests/maverick-modules/maverick_analysis/manifests/grafana.pp
+++ manifests/maverick-modules/maverick_analysis/manifests/grafana.pp
@@ -1,7 +1,7 @@
 class maverick_analysis::grafana (
     $webport = "6790",
     $rootpath = "/analysis/grafana/",
-    $grafana_version = "4.3.2",
+    $grafana_version = "4.5.2",
 ) {
     
     ensure_packages(["sqlite3"])
