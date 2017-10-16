6b35c311e35af240cb8757e14f501f3e665607ed
fnoop <fnoopdogg@gmail.com>
2017-10-16 12:55:04 +0100

    Disable root password change by default

--- manifests/maverick-modules/base/manifests/users.pp
+++ manifests/maverick-modules/base/manifests/users.pp
@@ -2,6 +2,7 @@ class base::users (
     $mav_password = '$6$YuXyoBZR$cR/cNLGZV.Y/nfW6rvK//fjnr84kckI1HM0fhPnJ3MVVlsl7UxaK8vSw.bM4vTlkF4RTbOSAdi36c5d2hJ9Gj1',
     $mav_sudopass = false, # If mav sudo should ask for password or not
     $root_password = '$6$MIBUXpXc$AA8j.88LvHFBzvVKofKcHnEqvWdv5Cl5D8.O8aB446Mao2X4UkuJ.1VKSr2VcmsbZB7A5ypmmkO0MWGAZr37N.',
+    $manage_root_password = false,
 ) {
     ensure_packages(["ruby-shadow"])
     
@@ -54,7 +55,10 @@ class base::users (
         }
     }
     
-    user { "root":
-        password    => "${root_password}",
+    if $manage_root_password == true {
+        user { "root":
+            password    => "${root_password}",
+        }
     }
+        
 }
