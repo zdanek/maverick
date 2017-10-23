b1b805fb03d289ffd9c9fb6209dcb77df9816941
fnoop <fnoopdogg@gmail.com>
2017-10-23 14:30:07 +0100

    Move libseek_thermal from hardware to vision module, #574

--- manifests/maverick-modules/maverick_hardware/manifests/peripheral/seekthermal.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/peripheral/seekthermal.pp
@@ -1,7 +1,4 @@
 class maverick_hardware::peripheral::seekthermal (
-    $libseek_thermal = true,
-    $libseek_thermal_source = "https://github.com/fnoop/libseek-thermal.git",
-    $libseek_thermal_revision = "master",
     $libseek = false,
     $seek_id = "0010", # 0010 for Compact, 0011 for Compact Pro
 ) {
@@ -19,45 +16,6 @@ class maverick_hardware::peripheral::seekthermal (
         group       => "root",
         mode        => "644",
         content     => "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"289d\", ATTRS{idProduct}==\"${$_seekid}\", MODE=\"0664\", OWNER=\"mav\", GROUP=\"mav\"\n",
-    }
-
-    # Install libseek-thermal
-    if $libseek_thermal == true {
-        ensure_packages(["libusb-1.0-0-dev", "libboost-program-options-dev"])
-        if ! ("install_flag_libseek-thermal" in $installflags) {
-            oncevcsrepo { "git-libseek-thermal":
-                gitsource   => $libseek_thermal_source,
-                dest        => "/srv/maverick/var/build/libseek-thermal",
-                revision    => $libseek_thermal_revision,
-            } ->
-            exec { "libseek-thermal-compile":
-                user        => "mav",
-                timeout     => 0,
-                environment => ["CXXFLAGS=-I/srv/maverick/software/opencv/include", "LDFLAGS=-L/srv/maverick/software/opencv/lib -Wl,-rpath=/srv/maverick/software/opencv/lib", "PKG_CONFIG_PATH=/srv/maverick/software/opencv/lib/pkgconfig"],
-                command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/libseek-thermal.build.log 2>&1",
-                cwd         => "/srv/maverick/var/build/libseek-thermal",
-                creates     => "/srv/maverick/var/build/libseek-thermal/lib/libseek.so",
-                require     => [ Package["libusb-1.0-0-dev"], Class["maverick_vision::opencv"] ],
-            } ->
-            exec { "libseek-thermal-install":
-                user        => "mav",
-                command     => "/usr/bin/make install PREFIX=/srv/maverick/software/libseek-thermal",
-                cwd         => "/srv/maverick/var/build/libseek-thermal",
-                creates     => "/srv/maverick/software/libseek-thermal/lib/libseek.so",
-                before      => Service_wrapper["maverick-vision_seek"],
-            } ->
-            file { "/srv/maverick/var/build/.install_flag_libseek-thermal":
-                ensure      => present,
-                owner       => mav,
-                mode        => "644"
-            } ->
-            file { "/etc/profile.d/70-maverick-libseek-thermal-pkgconfig.sh":
-                mode        => "644",
-                owner       => "root",
-                group       => "root",
-                content     => "export PKG_CONFIG_PATH=/srv/maverick/software/libseek-thermal/lib/pkgconfig:\$PKG_CONFIG_PATH",
-            }
-        }
     }
     
     # Install libseek
--- manifests/maverick-modules/maverick_vision/manifests/vision_seek.pp
+++ manifests/maverick-modules/maverick_vision/manifests/vision_seek.pp
@@ -1,7 +1,49 @@
 class maverick_vision::vision_seek (
     $active = false,
+    $libseek_thermal = true,
+    $libseek_thermal_source = "https://github.com/fnoop/libseek-thermal.git",
+    $libseek_thermal_revision = "master",
 ) {
     
+    # Install libseek-thermal
+    if $libseek_thermal == true {
+        ensure_packages(["libusb-1.0-0-dev", "libboost-program-options-dev"])
+        if ! ("install_flag_libseek-thermal" in $installflags) {
+            oncevcsrepo { "git-libseek-thermal":
+                gitsource   => $libseek_thermal_source,
+                dest        => "/srv/maverick/var/build/libseek-thermal",
+                revision    => $libseek_thermal_revision,
+            } ->
+            exec { "libseek-thermal-compile":
+                user        => "mav",
+                timeout     => 0,
+                environment => ["CXXFLAGS=-I/srv/maverick/software/opencv/include", "LDFLAGS=-L/srv/maverick/software/opencv/lib -Wl,-rpath=/srv/maverick/software/opencv/lib", "PKG_CONFIG_PATH=/srv/maverick/software/opencv/lib/pkgconfig"],
+                command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/libseek-thermal.build.log 2>&1",
+                cwd         => "/srv/maverick/var/build/libseek-thermal",
+                creates     => "/srv/maverick/var/build/libseek-thermal/lib/libseek.so",
+                require     => [ Package["libusb-1.0-0-dev"], Class["maverick_vision::opencv"] ],
+            } ->
+            exec { "libseek-thermal-install":
+                user        => "mav",
+                command     => "/usr/bin/make install PREFIX=/srv/maverick/software/libseek-thermal",
+                cwd         => "/srv/maverick/var/build/libseek-thermal",
+                creates     => "/srv/maverick/software/libseek-thermal/lib/libseek.so",
+                before      => Service_wrapper["maverick-vision_seek"],
+            } ->
+            file { "/srv/maverick/var/build/.install_flag_libseek-thermal":
+                ensure      => present,
+                owner       => mav,
+                mode        => "644"
+            } ->
+            file { "/etc/profile.d/70-maverick-libseek-thermal-pkgconfig.sh":
+                mode        => "644",
+                owner       => "root",
+                group       => "root",
+                content     => "export PKG_CONFIG_PATH=/srv/maverick/software/libseek-thermal/lib/pkgconfig:\$PKG_CONFIG_PATH",
+            }
+        }
+    }
+
     # Temp location/copy of files
     file { "/srv/maverick/software/vision_seek":
         ensure  => directory,
