895960c09854b68ac1eb4881af837bec511fbf65
fnoop <fnoopdogg@gmail.com>
2017-10-24 10:17:43 +0100

    Remove java-common from raspberry package remove #574

--- manifests/maverick-modules/maverick_hardware/manifests/raspberry.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/raspberry.pp
@@ -37,7 +37,7 @@ class maverick_hardware::raspberry (
         if ! getvar("maverick_intelligence::tensorflow") == true {
             notice("Removing Raspberry java packages")
             exec { "raspberry-remove-java":
-                command     => "/usr/bin/apt purge -y oracle-java8-jdk oracle-java7-jdk openjdk-8-jdk* openjdk-8-jre* openjdk-7-jdk* openjdk-7-jre* java-common libservlet2.5-java",
+                command     => "/usr/bin/apt purge -y oracle-java8-jdk oracle-java7-jdk openjdk-8-jdk* openjdk-8-jre* openjdk-7-jdk* openjdk-7-jre* libservlet2.5-java",
                 onlyif      => "/usr/bin/dpkg -s openjdk-8-jdk || /usr/bin/dpkg -s openjdk-7-jdk",
             }
         }
