16b5b16d2636d33e3510e874b99229343713f1f0
fnoop <fnoopdogg@gmail.com>
2017-11-13 08:53:47 +0000

    Fix circular symlinks, #661

--- manifests/maverick-modules/maverick_web/manifests/nodejs.pp
+++ manifests/maverick-modules/maverick_web/manifests/nodejs.pp
@@ -3,7 +3,7 @@ class maverick_web::nodejs (
     
     class { 'nodejs':
         repo_url_suffix => '8.x',
-        legacy_debian_symlinks => true,
+        legacy_debian_symlinks => false,
         nodejs_package_ensure => latest,
     }
 
