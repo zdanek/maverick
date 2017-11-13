2f7365b6458983d826796b2796b00b22d6b3283e
fnoop <fnoopdogg@gmail.com>
2017-11-13 08:44:37 +0000

    Update maverick to new docs location, #650

--- manifests/maverick-modules/maverick_web/manifests/maverick_docs.pp
+++ manifests/maverick-modules/maverick_web/manifests/maverick_docs.pp
@@ -1,18 +1,24 @@
 class maverick_web::maverick_docs (
 ) {
     
+    /*
     # Install maverick docs
     oncevcsrepo { "github-maverick_docs":
         gitsource   => "https://github.com/fnoop/maverick",
         dest        => "/srv/maverick/software/maverick-docs",
         revision    => "gh-pages",
     }
+    */
+    file { "/srv/maverick/software/maverick-docs":
+        ensure      => absent,
+        force       => true,
+    }
 
     nginx::resource::location { "web-maverick-docs":
         ensure          => present,
         ssl             => true,
         location        => "/web/maverick-docs",
-        location_alias  => "/srv/maverick/software/maverick-docs",
+        location_alias  => "/srv/maverick/software/maverick/docs",
         index_files     => ["maverick.html"], # index.html is for github hosted docs
         server          => "${::hostname}.local",
         require         => [ Class["maverick_gcs::fcs"], Class["nginx"] ],
