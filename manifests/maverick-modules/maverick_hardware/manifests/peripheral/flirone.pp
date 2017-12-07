c90919a08fade4e472a2bde8e9bacf5cb31921ab
fnoop <fnoopdogg@gmail.com>
2017-12-07 22:46:45 +0000

    Move flir to ~software #472

--- manifests/maverick-modules/maverick_hardware/manifests/peripheral/flirone.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/peripheral/flirone.pp
@@ -4,12 +4,12 @@ class maverick_hardware::peripheral::flirone (
     ensure_packages(["raspberrypi-kernel-headers", "libusb-1.0-0-dev", "dkms", "v4l2loopback-utils", "v4l2loopback-dkms"])
     oncevcsrepo { "git-libseek":
         gitsource   => "https://github.com/fnoop/flirone-v4l2.git",
-        dest        => "/srv/maverick/var/build/flirone-v4l2",
+        dest        => "/srv/maverick/software/flirone",
     } ->
     exec { "flirone-compile":
         command     => "/usr/bin/make",
-        cwd         => "/srv/maverick/var/build/flirone-v4l2",
-        creates     => "/srv/maverick/var/build/flirone-v4l2/flirone",
+        cwd         => "/srv/maverick/software/flirone",
+        creates     => "/srv/maverick/software/flirone/flirone",
         require     => [ Package["v4l2loopback-dkms"], Package["libusb-1.0-0-dev"] ],
     }
     
