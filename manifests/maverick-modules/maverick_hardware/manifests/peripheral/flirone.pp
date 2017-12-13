dbc4bacb675d9e0623771073274b1344a0493f92
fnoop <fnoopdogg@gmail.com>
2017-12-13 08:31:16 +0000

    Fix flirone raspberry dependencies

--- manifests/maverick-modules/maverick_hardware/manifests/peripheral/flirone.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/peripheral/flirone.pp
@@ -2,7 +2,12 @@ class maverick_hardware::peripheral::flirone (
     $active = false,
 ) {
     
-    ensure_packages(["raspberrypi-kernel-headers", "libusb-1.0-0-dev", "dkms", "v4l2loopback-utils", "v4l2loopback-dkms"])
+    # Install dependencies
+    if $::raspberry_present == "yes" {
+        ensure_packages(["raspberrypi-kernel-headers"])
+    }
+    ensure_packages(["libusb-1.0-0-dev", "dkms", "v4l2loopback-utils", "v4l2loopback-dkms"])
+    
     oncevcsrepo { "git-libseek":
         gitsource   => "https://github.com/fnoop/flirone-v4l2.git",
         dest        => "/srv/maverick/software/flirone",
