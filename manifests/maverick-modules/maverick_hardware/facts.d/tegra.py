2ab6da684875924cb653194b6c933a60972e2c80
fnoop <fnoopdogg@gmail.com>
2017-12-17 17:55:57 +0000

    Add tegra fact, info, maverick-tegra sample node, #706

---
+++ manifests/maverick-modules/maverick_hardware/facts.d/tegra.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# This fact extracts as much hardware information out of a Tegra board as possible
+
+import os, re, sys, subprocess
+
+class Tegra(object):
+    def __init__(self):
+        self.data = {'present': 'no'}
+        
+    def boarddata(self):
+        count = 0
+        
+        try:
+            if os.path.isdir("/sys/module/tegra_fuse/parameters"):
+                self.data['vendor'] = 'Nvidia'
+                self.data['present'] = 'yes'
+            else:
+                self.data['vendor'] = None
+        except:
+            pass
+        
+        try:
+            with open('/sys/module/tegra_fuse/parameters/tegra_chip_id', 'r') as f:
+                board = f.readline().strip()
+                if board == "64":
+                    self.data['model'] = 'TK1'
+                elif board == "33":
+                    self.data['model'] = 'TX1'
+                elif board == "24":
+                    self.data['model'] = 'TX2'
+                else:
+                    self.data['model'] = None
+        except:
+            pass
+        
+        # Define main data container
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            r = re.search('^(.*)\s+:\s+(.*)', line)
+            if not r or not r.groups(0) or not r.groups(0)[0] or not r.groups(0)[1]: continue
+            (key,val) = r.groups(0)[0],r.groups(0)[1]
+            if key == "processor":
+                count += 1
+        self.data['cpucores'] = count
+        f.close()
+        f = open('/proc/meminfo', 'r')
+        for line in f:
+            r = re.search('^(.*):\s+(.*)', line)
+            if not r or not r.groups(0) or not r.groups(0)[0] or not r.groups(0)[1]: continue
+            (key,val) = r.groups(0)[0],r.groups(0)[1]
+            if key == "MemTotal": 
+                self.data['memory'] = val
+            elif key == "SwapTotal":
+                self.data['swap'] = val
+        f.close()
+
+    def storagedata(self):
+        # Obtain the SD card size from proc
+        f = open('/proc/partitions', 'r')
+        for line in f:
+            if re.search("mmcblk[01]$", line):
+                self.data['sdsize'] = int(line.split()[2]) / 1000
+        f.close()
+        
+    def runall(self):
+        self.boarddata()
+        self.storagedata()
+
+#If we're being called as a command, instantiate and report
+if __name__ == '__main__':
+    tegra = Tegra()
+    tegra.boarddata()
+    if tegra.data['present'] == "no":
+        print "tegra_present=no"
+        sys.exit(1)
+    tegra.storagedata()
+
+    # Finally, print the data out in the format expected of a fact provider
+    if tegra.data:
+        for key,val in tegra.data.items():
+            print "tegra_%s=%s" % (key, val)
--- manifests/maverick-modules/maverick_security/manifests/firewall.pp
+++ manifests/maverick-modules/maverick_security/manifests/firewall.pp
@@ -6,50 +6,50 @@ class maverick_security::firewall (
     resources { "firewall":
         purge => true,
     }
-	
-	Firewall {
-		before  => Class['maverick_security::firewall_post'],
-		require => Class['maverick_security::firewall_pre'],
-	}
-	
+
+    Firewall {
+        before  => Class['maverick_security::firewall_post'],
+        require => Class['maverick_security::firewall_pre'],
+    }
+
     class { ['maverick_security::firewall_pre', 'maverick_security::firewall_post']:
     }
-   	
+
     class { '::firewall':
-    	ensure		=> running,
-    	require		=> Package["cups-filters"] # ensure cups-filters is removed as it can stop iptables working
+        ensure		=> running,
+        require		=> Package["cups-filters"] # ensure cups-filters is removed as it can stop iptables working
     }
-    
+
 #    service_wrapper { "netfilter-persistent":
 #    	ensure		=> running,
 #    	enable		=> true,
 #    }
-   	
+
     ### Define some common rules across all servers
     include maverick_security::firewall_common
-    
+
     ### If set, then define a cron to update iptables from puppet once an hour.  This is useful for dyndns.
     if $cronupdate == true {
         cron::job { "puppet-firewall":
-	    	minute		=> "0",
-	    	hour        => "*",
-	    	date        => "*",
-	    	month       => "*",
-	    	weekday     => "*",
-		 	user		=> "root",
-	    	command		=> '/bin/sleep `expr ${RANDOM} \% 600`; puppet agent --tags maverick_security::firewall -t >/var/log/puppet-firewall 2>&1'
-	    }
+            minute		=> "0",
+            hour        => "*",
+            date        => "*",
+            month       => "*",
+            weekday     => "*",
+            user		=> "root",
+            command		=> '/bin/sleep `expr ${RANDOM} \% 600`; puppet agent --tags maverick_security::firewall -t >/var/log/puppet-firewall 2>&1'
+        }
     }
 
     define firerule ($ports, $ips, $proto = "tcp") {
-    	any2array($ports).each |$port| {
-			firewall { "100 allow ${name} access for ${proto}:${port} from ${ips}":
-				dport       => $port,
-				proto       => $proto,
-				action      => accept,
-				source      => $ips,
-			}
-    	}
-	}
+        any2array($ports).each |$port| {
+            firewall { "100 allow ${name} access for ${proto}:${port} from ${ips}":
+                dport       => $port,
+                proto       => $proto,
+                action      => accept,
+                source      => $ips,
+            }
+        }
+    }
     
 }
