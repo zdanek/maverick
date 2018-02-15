c99f94d1432c6a66d1ee39bcfe0f56af4045a040
fnoop <fnoopdogg@gmail.com>
2018-02-15 14:06:45 +0000

    Refactor python deps

--- manifests/maverick-modules/base/manifests/python.pp
+++ manifests/maverick-modules/base/manifests/python.pp
@@ -7,25 +7,20 @@ class base::python (
         dev        => 'present',
         virtualenv => 'present',
         gunicorn   => 'absent',
-    } ->
-    # Install python packages
-    package { ["python-setuptools", "virtualenvwrapper", "python-numpy", "python-lockfile", "python-daemon"]:
-        ensure      => present,
-    } ->
-    # Install python3 packages
-    package { ["python3-numpy", "python3-yaml"]:
-        ensure      => present,
-    } ->
-    # Install pylint for cloud9 linting
-    package { ["pylint", "pylint3"]:
-        ensure      => present,
+        before     => Package["python-setuptools"],
     }
-    
+    ensure_packages(["python-setuptools", "virtualenvwrapper", "python-numpy", "python-lockfile", "python-daemon"])
+    # Install python3 packages
+    ensure_packages(["python3-numpy", "python3-yaml"])
+    # Install pylint for cloud9 linting
+    ensure_packages(["pylint", "pylint3"])
+
     # Need to install/upgrade pip to a known version using easy_install, which is the only method that works reliably.
     exec { "upgrade-pip":
         command     => "sudo easy_install -U pip==9.0.1",
         unless      => "ls /usr/local/lib/python2.7/dist-packages/pip-9.0.1*.egg",
         path        => ["/usr/local/bin", "/usr/bin", "/bin"],
+        require     => Package["python-setuptools"],
     } ->
     # Install PyRIC and netifaces, python modules necessary to run maverick --netinfo
     install_python_module { 'pip-pyric':
