285b566301ab48203896bbea94f90c813de61dd6
fnoop <fnoopdogg@gmail.com>
2018-02-25 12:16:08 +0000

    Add new maverick-web paths

--- manifests/maverick-modules/maverick_web/manifests/maverick_web.pp
+++ manifests/maverick-modules/maverick_web/manifests/maverick_web.pp
@@ -1,6 +1,8 @@
 class maverick_web::maverick_web (
     $webport = 6794,
     $active = true,
+    $webpath_dev = '/dev/maverick/',
+    $webpath_prod = '/web/maverick',
     $server_hostname = $maverick_web::server_fqdn,
     $auth_message = undef,
     $auth_file = undef,
@@ -34,7 +36,7 @@ class maverick_web::maverick_web (
         notify      => Exec["maverick-systemctl-daemon-reload"],
     } -> 
     nginx::resource::location { "maverick-web":
-        location    => "/maverick-web/",
+        location    => $webpath_dev,
         proxy       => "http://localhost:${webport}/",
         server      => $server_hostname,
         auth_basic  => $auth_message,
@@ -42,7 +44,7 @@ class maverick_web::maverick_web (
         require     => [ Class["maverick_gcs::fcs"], Class["nginx"] ],
     } ->
     nginx::resource::location { "maverick-web-prod":
-        location    => "/web/maverick",
+        location    => $webpath_prod,
         ensure          => present,
         ssl             => true,
         location_alias  => "/srv/maverick/code/maverick-web/dist",
@@ -50,7 +52,23 @@ class maverick_web::maverick_web (
         server          => $server_hostname,
         require         => [ Class["maverick_gcs::fcs"], Class["nginx"] ],
     }
-
+    /*
+    nginx::resource::location { "maverick-web-prod-precache":
+        location    => "~ (index.html|service-worker.js)$",
+        raw_append  => [
+            "add_header Last-Modified \$date_gmt;",
+            "add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';",
+            "if_modified_since off;",
+            "expires off;",
+            "etag off;"
+        ],
+        index_files     => [],
+        server          => $server_hostname,
+        ssl             => true,
+        location_alias  => "/srv/maverick/code/maverick-web/dist/",
+    }
+    */
+    
     if $active == true {
         service { "maverick-web":
             ensure      => running,
