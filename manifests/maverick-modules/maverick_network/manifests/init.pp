4093dba416af2eec35a647f1d70149616bb3475e
fnoop <fnoopdogg@gmail.com>
2018-05-01 12:24:13 +0100

    Fix resolv.conf symlink, #767

--- manifests/maverick-modules/maverick_network/manifests/init.pp
+++ manifests/maverick-modules/maverick_network/manifests/init.pp
@@ -195,7 +195,7 @@ class maverick_network (
         # Reset resolv.conf to resolvconf
         file { "/etc/resolv.conf":
             ensure      => symlink,
-            target      => "/run/resolvconf/resolv.conf",
+            target      => "/etc/resolvconf/run/resolv.conf",
         }
     }
     
