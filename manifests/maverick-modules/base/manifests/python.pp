cce9552c492ce61a6d40bf3983a59d0cdd5276a0
fnoop <fnoopdogg@gmail.com>
2018-06-01 12:01:56 +0100

    Fix easy_install pip update, #670

--- manifests/maverick-modules/base/manifests/python.pp
+++ manifests/maverick-modules/base/manifests/python.pp
@@ -18,6 +18,7 @@ class base::python (
     # Need to install/upgrade pip to a known version using easy_install, which is the only method that works reliably.
     exec { "upgrade-pip":
         command     => "sudo easy_install -U pip==9.0.3",
+        onlyif      => "which easy_install",
         unless      => "ls /usr/local/lib/python2.7/dist-packages/pip-9.0.3*.egg",
         path        => ["/usr/local/bin", "/usr/bin", "/bin"],
         require     => Package["python-setuptools"],
