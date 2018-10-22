2445c66c828157f7f69491063d0b7a7f427e5382
fnoop <fnoopdogg@gmail.com>
2018-10-22 11:55:55 +0100

    Move to opencv 4, optimise builds, add openvino #790 #791

--- manifests/maverick-modules/maverick_vision/manifests/opencv.pp
+++ manifests/maverick-modules/maverick_vision/manifests/opencv.pp
@@ -1,17 +1,17 @@
 class maverick_vision::opencv (
     $contrib = true,
-    $opencv_version = "3.4.3",
+    $opencv_version = "4.0.0-beta",
     $release = "Release", # Release or Debug, OpenCV build type
     $precompile_headers = false,
     $armv7l_optimize = false,
+    $opencv_dldt_version = "2018_R3",
+    $opencv_omz_version = "2018_R3",
+    $openvino = true,
 ) {
     
-    # Ensure gstreamer resources are applied before this class
+    # Ensure gstreamer, tbb and openblas resources are applied before this class
     require maverick_vision::gstreamer
-    
-    # Compile opencv3, note this can take a while.
-    # Note that we're deliberately not installing the python bindings into either sitl or fc python virtualenvs
-    # as we want to access from both, so we install into global.
+    require maverick_vision::visionlibs
     
     # Install dependencies
     if $operatingsystem == "Ubuntu" and ($operatingsystemrelease == "17.04" or $operatingsystemrelease == "17.10" or $operatingsystemrelease == "18.04") {
@@ -22,10 +22,12 @@ class maverick_vision::opencv (
     ensure_packages(["libjpeg-dev", "libtiff5-dev", "libgdal-dev", "libavcodec-dev", "libavformat-dev", "libswscale-dev", "libv4l-dev", "libxvidcore-dev", "libatlas-base-dev", "gfortran", "libeigen3-dev", "libavresample-dev", "libopenblas-dev", "libgdcm2-dev", "liblapacke-dev", "libgtk2.0-dev"])
     ensure_packages(["python2.7-dev", "libpython3-all-dev"])
     ensure_packages(["libgtk2.0-dev"])
+    /*
     if $::hardwaremodel == "x86_64" { # https://github.com/goodrobots/maverick/issues/233
-        ensure_packages(["libtbb-dev", "libtbb2"])
+        ensure_packages(["libtbb-dev", "libtbb2"], {'ensure' => 'absent'})
     }
-    
+    */
+
     # If ~/var/build/.install_flag_opencv exists, skip pulling source and compiling
     if ! ("install_flag_opencv" in $installflags) {
         # Pull opencv and opencv_contrib from git
@@ -72,11 +74,16 @@ class maverick_vision::opencv (
             $_armopts = "-DENABLE_NEON=OFF -DENABLE_VFPV3=OFF"
         }
         
-        if $::hardwaremodel == "x86_64" {
-            # $_command           = "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv -DCMAKE_BUILD_TYPE=${release} ${_pchstr} -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON ${contribstr} -DBUILD_EXAMPLES=ON -DWITH_LIBV4L=OFF -DWITH_EIGEN=ON -DWITH_OPENGL=ON -DENABLE_OPENGL=ON -DENABLE_NEON=ON -DWITH_TBB=ON -DBUILD_TBB=ON -DENABLE_VFPV3=ON -DWITH_IPP=ON -DWITH_OPENVX=ON -DWITH_INTELPERC=ON -DWITH_VA=ON -DWITH_VA_INTEL=ON -DWITH_GSTREAMER=ON -DWITH_QT=OFF -DWITH_OPENNI2=ON -DENABLE_FAST_MATH=ON -DENABLE_SSE41=ON -DENABLE_SSE42=ON -DOPENVX_ROOT=/opt/intel/computer_vision_sdk_2017.0.087/openvx -DWITH_OPENVX=YES .. >/srv/maverick/var/log/build/opencv.cmake.out 2>&1"
-            $_command           = "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv -DCMAKE_BUILD_TYPE=${release} ${_pchstr} -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON ${contribstr} -DBUILD_EXAMPLES=ON -DWITH_LIBV4L=OFF -DWITH_EIGEN=ON -DWITH_OPENGL=ON -DENABLE_OPENGL=ON -DENABLE_NEON=ON -DWITH_TBB=ON -DBUILD_TBB=ON -DENABLE_VFPV3=ON -DWITH_IPP=ON -DWITH_OPENVX=ON -DWITH_INTELPERC=ON -DWITH_VA=ON -DWITH_VA_INTEL=ON -DWITH_GSTREAMER=ON -DWITH_QT=OFF -DWITH_OPENNI2=ON -DENABLE_FAST_MATH=ON -DENABLE_SSE41=ON -DENABLE_SSE42=ON .. >/srv/maverick/var/log/build/opencv.cmake.out 2>&1"
+        if $maverick_vision::visionlibs::tbb == true {
+            $_tbbopts = "-DWITH_TBB=ON -DBUILD_TBB=OFF"
         } else {
-            $_command           = "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv -DCMAKE_BUILD_TYPE=${release} ${_pchstr} -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON ${contribstr} -DBUILD_EXAMPLES=ON -DWITH_LIBV4L=OFF -DWITH_EIGEN=ON -DWITH_OPENGL=ON -DENABLE_OPENGL=ON -DWITH_TBB=OFF -DBUILD_TBB=OFF -DWITH_GSTREAMER=ON -DWITH_QT=OFF -DWITH_OPENNI2=ON ${_armopts} .. >/srv/maverick/var/log/build/opencv.cmake.out 2>&1"
+            $_tbbopts = "-DWITH_TBB=OFF -DBUILD_TBB=OFF"
+        }
+        
+        if $::hardwaremodel == "x86_64" {
+            $_command           = "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv -DCMAKE_BUILD_TYPE=${release} ${_pchstr} -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON ${contribstr} -DBUILD_EXAMPLES=ON -DWITH_LIBV4L=OFF -DWITH_EIGEN=ON -DWITH_OPENGL=ON -DENABLE_OPENGL=ON -DWITH_IPP=ON -DWITH_OPENVX=ON -DWITH_INTELPERC=ON -DWITH_VA=ON -DWITH_VA_INTEL=ON -DWITH_GSTREAMER=ON -DWITH_QT=OFF -DWITH_OPENNI2=ON -DENABLE_FAST_MATH=ON ${_tbbopts} .. >/srv/maverick/var/log/build/opencv.cmake.out 2>&1"
+        } else {
+            $_command           = "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv -DCMAKE_BUILD_TYPE=${release} ${_pchstr} -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON ${contribstr} -DBUILD_EXAMPLES=ON -DWITH_LIBV4L=OFF -DWITH_EIGEN=ON -DWITH_OPENGL=ON -DENABLE_OPENGL=ON -DWITH_GSTREAMER=ON -DWITH_QT=OFF -DWITH_OPENNI2=ON ${_armopts} ${_tbbopts} .. >/srv/maverick/var/log/build/opencv.cmake.out 2>&1"
         }
     
         if Numeric($memorysize_mb) < 1000 {
@@ -89,11 +96,19 @@ class maverick_vision::opencv (
         exec { "opencv-prepbuild":
             user        => "mav",
             timeout     => 0,
-            environment => ["PKG_CONFIG_PATH=/srv/maverick/software/gstreamer/lib/pkgconfig"],
+            environment => [
+                "PKG_CONFIG_PATH=/srv/maverick/software/gstreamer/lib/pkgconfig",
+                "TBB_LIB_DIR=/srv/maverick/software/tbb/lib",
+                "LIBRARY_PATH=/srv/maverick/software/tbb/lib",
+                "TBBROOT=/srv/maverick/software/tbb",
+                "CPATH=/srv/maverick/software/tbb/include",
+                "OpenBLAS_HOME=/srv/maverick/software/openblas",
+            ],
             command     => $_command,
             cwd         => "/srv/maverick/var/build/opencv/build",
             creates     => "/srv/maverick/var/build/opencv/build/Makefile",
-            require     => [ Class["maverick_vision::gstreamer"], File["/srv/maverick/var/build/opencv/build"], Package["libjpeg-dev", "libtiff5-dev", "libavcodec-dev", "libavformat-dev", "libswscale-dev", "libv4l-dev", "libxvidcore-dev", "libatlas-base-dev", "gfortran", "libeigen3-dev", "libavresample-dev", "libopenblas-dev", "libgdal-dev", "libgdcm2-dev", "liblapacke-dev", "libgtk2.0-dev", "python2.7-dev", "libpython3-all-dev"] ], # ensure we have all the dependencies satisfied
+            require     => [ Class["maverick_vision::visionlibs"], Class["maverick_vision::gstreamer"], File["/srv/maverick/var/build/opencv/build"], Package["libjpeg-dev", "libtiff5-dev", "libavcodec-dev", "libavformat-dev", "libswscale-dev", "libv4l-dev", "libxvidcore-dev", "libatlas-base-dev", "gfortran", "libeigen3-dev", "libavresample-dev", "libopenblas-dev", "libgdal-dev", "libgdcm2-dev", "liblapacke-dev", "libgtk2.0-dev", "python2.7-dev", "libpython3-all-dev"] ], # ensure we have all the dependencies satisfied
+            before      => Exec["opencv_dldt-prepbuild"],
         } ->
         exec { "opencv-build":
             user        => "mav",
@@ -153,6 +168,49 @@ class maverick_vision::opencv (
         owner       => "root",
         group       => "root",
         content     => "export CMAKE_PREFIX_PATH=/srv/maverick/software/opencv:\$CMAKE_PREFIX_PATH",
+    }
+
+    if $openvino == true {
+        # If ~/var/build/.install_flag_opencv_dldt exists, skip pulling source and compiling
+        if ! ("install_flag_opencv_dldt" in $installflags) {
+            oncevcsrepo { "git-opencv_dldt":
+                gitsource   => "https://github.com/opencv/dldt.git",
+                dest        => "/srv/maverick/var/build/opencv_dldt",
+                revision    => $opencv_dldt_version,
+            } ->
+            # Create build directory
+            file { "/srv/maverick/var/build/opencv_dldt/inference-engine/build":
+                ensure      => directory,
+                owner       => "mav",
+                group       => "mav",
+                mode        => "755",
+            } ->
+            exec { "opencv_dldt-prepbuild":
+                user        => "mav",
+                timeout     => 0,
+                command     => "/usr/bin/cmake -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/opencv_dldt -DCMAKE_BUILD_TYPE=Release ..",
+                cwd         => "/srv/maverick/var/build/opencv_dldt/inference-engine/build",
+                creates     => "/srv/maverick/var/build/opencv_dldt/inference-engine/build/Makefile",
+            } ->
+            exec { "opencv_dldt-build":
+                user        => "mav",
+                timeout     => 0,
+                command     => "/usr/bin/make >/srv/maverick/var/log/build/opencv_dldt.build.out 2>&1",
+                cwd         => "/srv/maverick/var/build/opencv_dldt/inference-engine/build",
+                #creates     => ,
+                #require     => Exec["opencv-prepbuild"],
+            } ->
+            exec { "opencv_dldt-install":
+                user        => "mav",
+                timeout     => 0,
+                command     => "/usr/bin/make install >/srv/maverick/var/log/build/opencv_dldt.install.out 2>&1",
+                cwd         => "/srv/maverick/var/build/opencv_dldt/inference-engine/build",
+                #creates     => $_install_creates,
+            } ->
+            file { "/srv/maverick/var/build/.install_flag_opencv_dldt":
+                ensure      => present,
+            }
+        }
     }
 
 }
