f3f57f2c5b179106e5e27c10993db98e5aae2fad
fnoop <fnoopdogg@gmail.com>
2018-11-12 22:55:32 +0000

    Remove bin/ symlinks from github

--- bin/api.sh
+++
@@ -1,1 +0,0 @@
-/srv/maverick/software/maverick/manifests/maverick-modules/maverick_web/files/api.sh
--- bin/px4sitl.sh
+++
@@ -1,1 +0,0 @@
-/srv/maverick/software/maverick/manifests/maverick-modules/maverick_dev/files/px4sitl.sh
