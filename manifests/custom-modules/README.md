ac8ce11e61801e4cce7f9be194a09a57da6931b7
fnoop <fnoopdogg@gmail.com>
2018-11-12 22:36:51 +0000

    Add custom module locations, #797

---
+++ manifests/custom-modules/README.md
@@ -0,0 +1,4 @@
+# Maverick custom-modules
+
+This is a location for custom modules to be developed.  It is .gitignore-d so anything added here won't be committed back to github.
+Alternatively, add custom modules to ~/code/maverick/custom-modules which is outside of Maverick github altogether, and you can add your own github projects within there, per module.
--- manifests/maverick-modules/base/manifests/maverick.pp
+++ manifests/maverick-modules/base/manifests/maverick.pp
@@ -5,7 +5,7 @@ class base::maverick (
    
    # Note: The mav user is setup in base::users
    
-   file { ["/srv/", "/srv/maverick", "/srv/maverick/software", "/srv/maverick/code", "/srv/maverick/data", "/srv/maverick/data/logs", "/srv/maverick/config", "/srv/maverick/config/maverick", "/srv/maverick/config/maverick/local-nodes", "/srv/maverick/var", "/srv/maverick/var/build", "/srv/maverick/var/log", "/srv/maverick/var/log/build", "/srv/maverick/var/log/maverick", "/srv/maverick/var/run", "/srv/maverick/var/lib"]:
+   file { ["/srv/", "/srv/maverick", "/srv/maverick/software", "/srv/maverick/code", "/srv/maverick/code/maverick", "/srv/maverick/code/maverick/custom-modules", "/srv/maverick/data", "/srv/maverick/data/logs", "/srv/maverick/config", "/srv/maverick/config/maverick", "/srv/maverick/config/maverick/local-nodes", "/srv/maverick/var", "/srv/maverick/var/build", "/srv/maverick/var/log", "/srv/maverick/var/log/build", "/srv/maverick/var/log/maverick", "/srv/maverick/var/run", "/srv/maverick/var/lib"]:
         ensure  => directory,
         owner   => "mav",
         group   => "mav",
