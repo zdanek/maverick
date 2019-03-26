6f75dc5e21c724703a2d06b15b2e2c884a90ace8
fnoop <fnoopdogg@gmail.com>
2019-03-26 22:55:58 +0000

    Move to saz-ssh puppet module #820

--- manifests/maverick-modules/maverick_security/manifests/ssh.pp
+++ manifests/maverick-modules/maverick_security/manifests/ssh.pp
@@ -15,14 +15,14 @@ class maverick_security::ssh {
         content         => template("maverick_security/issue.erb"),
     }
     class { "::ssh": 
-        sshd_config_print_motd      => 'no',
-        sshd_config_banner          => "/etc/issue.net",
-        permit_root_login           => 'yes',
-        ssh_config_forward_x11_trusted  => 'yes',
-        sshd_x11_forwarding         => 'yes',
-        sshd_password_authentication    => 'yes',
-        ssh_key_import              => false,
-        #ssh_key_export              => false,
+        storeconfigs_enabled    => false,
+        server_options          => {
+            'PasswordAuthentication' => 'yes',
+            'AllowTcpForwarding' => 'no',
+            'X11Forwarding' => 'no',
+            'Banner' => '/etc/issue.net',
+            'PrintMotd' => 'no',
+        }
     }
         
 }
