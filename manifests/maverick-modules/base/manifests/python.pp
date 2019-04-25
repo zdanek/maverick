75d52d63ea2fd344881b692907ad0457ca6814e0
fnoop <fnoopdogg@gmail.com>
2019-04-25 09:50:52 +0100

    Add bz2 and sqlite support to python ubild #809

--- manifests/maverick-modules/base/manifests/python.pp
+++ manifests/maverick-modules/base/manifests/python.pp
@@ -9,7 +9,7 @@ class base::python (
         if ! ("install_flag_python" in $installflags) {
             warning("Optimized Python3 will be built and can take a long time, please be patient..")
             # Pull python from git
-            ensure_packages(["libffi-dev", "libncurses5-dev", "libncursesw5-dev", "libgdbm-dev", "libgdbm-compat-dev", "libreadline-dev", "tk-dev", "liblzma-dev"])
+            ensure_packages(["libffi-dev", "libncurses5-dev", "libncursesw5-dev", "libgdbm-dev", "libgdbm-compat-dev", "libreadline-dev", "tk-dev", "liblzma-dev", "libbz2-dev", "libsqlite3-dev"])
             oncevcsrepo { "git-python":
                 gitsource   => "https://github.com/python/cpython.git",
                 dest        => "/srv/maverick/var/build/python",
@@ -22,7 +22,7 @@ class base::python (
                 creates     => "/srv/maverick/var/build/python/Makefile",
                 user        => "mav",
                 timeout     => 0,
-                require     => [ Package["libffi-dev"], Package["tk-dev"], ],
+                require     => [ Package["libffi-dev"], Package["tk-dev"], Package["libbz2-dev"], ],
             } ->
             exec { "python-make":
                 environment => ["LDFLAGS=-Wl,-rpath,/srv/maverick/software/python/lib"],
@@ -43,6 +43,7 @@ class base::python (
             exec { "python-pip-upgrade":
                 cwd         => "/srv/maverick/software/python",
                 command     => "/srv/maverick/software/python/bin/pip3 install --upgrade pip",
+                user        => "mav",
             } ->
             file { "/srv/maverick/var/build/.install_flag_python":
                 ensure      => present,
