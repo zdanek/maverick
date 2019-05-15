b25b5ee19cb370a81af44efc95243bb62693e280
fnoop <fnoopdogg@gmail.com>
2019-05-15 11:06:59 +0100

    Reorder openldap manifest resources #843

--- manifests/maverick-modules/maverick_security/manifests/ldap_server.pp
+++ manifests/maverick-modules/maverick_security/manifests/ldap_server.pp
@@ -15,52 +15,6 @@ class maverick_security::ldap_server (
     # Retrieve CA passphrase for signing
     $ca_passphrase = getvar("maverick_security::ssl::ca_passphrase")
 
-    # Create ldap ssl and data directory
-    file { ["/etc/ldap/ssl"]:
-    	ensure		=> directory,
-    	owner		=> "openldap",
-    	group		=> "openldap",
-    	mode		=> "755",
-    } ->
-
-    # Create cert chain
-    exec { "create-ldapssl-key":
-        command     => "/usr/bin/openssl genrsa -out /etc/ldap/ssl/${cert_cname}-ldapssl.key 2048",
-        creates     => "/etc/ldap/ssl/${cert_cname}-ldapssl.key",
-        user        => "openldap",
-    } ->
-    exec { "create-ldapssl-csr":
-        command     => "/usr/bin/openssl req -new -key /etc/ldap/ssl/${cert_cname}-ldapssl.key -out /etc/ldap/ssl/${cert_cname}-ldapssl.csr -subj \"/C=${cert_country}/ST=${cert_state}/L=${cert_locality}/O=${cert_orgname}/OU=${cert_orgunit}/CN=${cert_cname}\"",
-        creates     => "/etc/ldap/ssl/${cert_cname}-ldapssl.csr",
-        user        => "openldap",
-    } ->
-    file { "/etc/ldap/ssl/${cert_cname}-ldapssl.ext":
-        ensure      => present,
-        content     => template("maverick_security/ldapssl.ext.erb"),
-        owner       => "openldap",
-        group       => "openldap",
-    } ->
-    exec { "create-ldapssl-cert":
-        command     => "/usr/bin/openssl x509 -req -passin pass:${ca_passphrase} -in /etc/ldap/ssl/${cert_cname}-ldapssl.csr -CA /srv/maverick/data/security/ssl/ca/mavCA.pem -CAkey /srv/maverick/data/security/ssl/ca/mavCA.key -CAcreateserial -out /etc/ldap/ssl/${cert_cname}-ldapssl.crt -days 365 -sha512 -extfile /etc/ldap/ssl/${cert_cname}-ldapssl.ext",
-        unless      => "/usr/bin/find /etc/ldap/ssl -name ${cert_cname}-ldapssl.crt -size +0 | /bin/egrep '.*'",
-        require     => Exec["create-ca-rootcert"],
-        notify      => Service["slapd"]
-    } ->
-
-    # Ensure resulting cert ownerships are correct
-    /*
-    exec { "chown-ldapssl":
-        command     => "/bin/chown -R openldap /etc/ldap/ssl/*",
-        onlyif      => "/bin/ls -l /etc/ldap/ssl/* |awk {'print $3'} |grep -v openldap",
-    }
-    */
-    file { [ "/etc/ldap/ssl/${cert_cname}-ldapssl.key", "/etc/ldap/ssl/${cert_cname}-ldapssl.csr", "/etc/ldap/ssl/${cert_cname}-ldapssl.crt" ]:
-        owner       => "openldap",
-        group       => "openldap",
-        mode        => "0644",
-    }
-    
-    
     # Dependency for augeas shellvar provider
     ensure_packages(["ruby-augeas"])
     
@@ -98,12 +52,60 @@ class maverick_security::ldap_server (
             ssl_key   => '/etc/ldap/ssl/slapd-ldapssl.key',
             require   => Package["ruby-augeas"],
         }
+
+        # Define main ldap database
         openldap::server::database { $base:
           ensure    => present,
           rootdn    => "cn=admin,${base}",
           rootpw    => $roothash,
           # directory => "/srv/maverick/data/security/ldap/maverick.one",
         }
+
+        # Create ldap ssl and data directory
+        file { ["/etc/ldap/ssl"]:
+            ensure		=> directory,
+            owner		=> "openldap",
+            group		=> "openldap",
+            mode		=> "755",
+        } ->
+
+        # Create cert chain
+        exec { "create-ldapssl-key":
+            command     => "/usr/bin/openssl genrsa -out /etc/ldap/ssl/${cert_cname}-ldapssl.key 2048",
+            creates     => "/etc/ldap/ssl/${cert_cname}-ldapssl.key",
+            user        => "openldap",
+        } ->
+        exec { "create-ldapssl-csr":
+            command     => "/usr/bin/openssl req -new -key /etc/ldap/ssl/${cert_cname}-ldapssl.key -out /etc/ldap/ssl/${cert_cname}-ldapssl.csr -subj \"/C=${cert_country}/ST=${cert_state}/L=${cert_locality}/O=${cert_orgname}/OU=${cert_orgunit}/CN=${cert_cname}\"",
+            creates     => "/etc/ldap/ssl/${cert_cname}-ldapssl.csr",
+            user        => "openldap",
+        } ->
+        file { "/etc/ldap/ssl/${cert_cname}-ldapssl.ext":
+            ensure      => present,
+            content     => template("maverick_security/ldapssl.ext.erb"),
+            owner       => "openldap",
+            group       => "openldap",
+        } ->
+        exec { "create-ldapssl-cert":
+            command     => "/usr/bin/openssl x509 -req -passin pass:${ca_passphrase} -in /etc/ldap/ssl/${cert_cname}-ldapssl.csr -CA /srv/maverick/data/security/ssl/ca/mavCA.pem -CAkey /srv/maverick/data/security/ssl/ca/mavCA.key -CAcreateserial -out /etc/ldap/ssl/${cert_cname}-ldapssl.crt -days 365 -sha512 -extfile /etc/ldap/ssl/${cert_cname}-ldapssl.ext",
+            unless      => "/usr/bin/find /etc/ldap/ssl -name ${cert_cname}-ldapssl.crt -size +0 | /bin/egrep '.*'",
+            require     => Exec["create-ca-rootcert"],
+            notify      => Service["slapd"]
+        } ->
+
+        # Ensure resulting cert ownerships are correct
+        /*
+        exec { "chown-ldapssl":
+            command     => "/bin/chown -R openldap /etc/ldap/ssl/*",
+            onlyif      => "/bin/ls -l /etc/ldap/ssl/* |awk {'print $3'} |grep -v openldap",
+        }
+        */
+        file { [ "/etc/ldap/ssl/${cert_cname}-ldapssl.key", "/etc/ldap/ssl/${cert_cname}-ldapssl.csr", "/etc/ldap/ssl/${cert_cname}-ldapssl.crt" ]:
+            owner       => "openldap",
+            group       => "openldap",
+            mode        => "0644",
+        }
+
         /*
         openldap::server::globalconf { 'LogLevel':
           ensure => present,
