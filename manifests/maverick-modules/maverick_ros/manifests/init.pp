85cd85e87f5f8a9ae82e3edb1dc297839b7aec2e
fnoop <fnoopdogg@gmail.com>
2019-08-08 23:36:37 +0100

    Another go at fixing ros keys..

--- manifests/maverick-modules/maverick_ros/manifests/init.pp
+++ manifests/maverick-modules/maverick_ros/manifests/init.pp
@@ -147,15 +147,15 @@ class maverick_ros (
             force       => true,
         } ->
         # Remove compromised ROS repo key
-        exec { "ros-repo-badkey":
-            command     => "/usr/bin/apt-key del '421C 365B D9FF 1F71 7815  A389 5523 BAEE B01F A116'",
-            onlyif      => "/usr/bin/apt-key list --fingerprint |/bin/grep 'A389 5523 BAEE B01F A116'",
+        apt::key { 'ros-repo-badkey':
+            id      => '421C365BD9FF1F717815A3895523BAEEB01FA116',
+            ensure  => absent,
             require     => Package["dirmngr"],
         } ->
         # Install ROS bootstrap from ros.org packages
-        exec { "ros-repo-key":
-            command     => "/usr/bin/apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654",
-            unless      => "/usr/bin/apt-key list --fingerprint |/bin/grep 'C1CF 6E31 E6BA DE88 68B1  72B4 F42E D6FB AB17 C654'",
+        apt::key { 'ros-repo-key':
+            id      => 'C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654',
+            server  => 'keyserver.ubuntu.com',
             require     => Package["dirmngr"],
             notify      => Exec["ros_apt_update"],            
         } ->
