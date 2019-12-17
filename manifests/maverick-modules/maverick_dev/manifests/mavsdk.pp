9e1e8b6a31f57822461ce79f402ecf80c7eef93f
fnoop <fnoopdogg@gmail.com>
2019-12-17 18:15:45 +0000

    Fix mavsdk build #885

--- manifests/maverick-modules/maverick_dev/manifests/mavsdk.pp
+++ manifests/maverick-modules/maverick_dev/manifests/mavsdk.pp
@@ -3,6 +3,9 @@ class maverick_dev::mavsdk (
 
     # Install px4 dev/build dependencies
     ensure_packages(["cmake", "build-essential", "colordiff", "astyle", "libcurl4-openssl-dev", "doxygen", "libgrpc-dev", "libgrpc++-dev"])
+    #package { ["libgrpc-dev", "libgrpc++-dev"]:
+    #    ensure => absent,
+    #}
 
     # Install mavsdk
     if ! ("install_flag_mavsdk" in $installflags) {
@@ -20,23 +23,26 @@ class maverick_dev::mavsdk (
         exec { "mavsdk-prepbuild":
             user        => "mav",
             timeout     => 0,
-            command     => "/usr/bin/cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBUILD_BACKEND=ON -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/mavsdk ../.. >/srv/maverick/var/log/build/mavsdk.cmake.out 2>&1",
-            cwd         => "/srv/maverick/var/build/mavsdk/build/default",
+            command     => "/usr/bin/cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBUILD_BACKEND=ON -DSUPERBUILD=ON -Bbuild/default -H. -DCMAKE_INSTALL_PREFIX=/srv/maverick/software/mavsdk -DDEPS_INSTALL_PATH=/srv/maverick/software/mavsdk -DCMAKE_INSTALL_RPATH=/srv/maverick/software/mavsdk/lib >/srv/maverick/var/log/build/mavsdk.cmake.out 2>&1",
+            cwd         => "/srv/maverick/var/build/mavsdk",
             creates     => "/srv/maverick/var/build/mavsdk/build/default/Makefile",
+            require     => [ Package["libgrpc-dev"], Package["libgrpc++-dev"] ],
         } ->
         exec { "mavsdk-build":
             user        => "mav",
             timeout     => 0,
-            command     => "/usr/bin/make -j${::processorcount} >/srv/maverick/var/log/build/mavsdk.build.out 2>&1",
-            cwd         => "/srv/maverick/var/build/mavsdk/build/default",
+            environment => ["LD_LIBRARY_PATH=/srv/maverick/var/build/mavsdk/build/default/third_party/install/lib"],
+            command     => "/usr/bin/cmake --build build/default 2>&1",
+            cwd         => "/srv/maverick/var/build/mavsdk",
             creates     => "/srv/maverick/var/build/mavsdk/build/default/src/core/libmavsdk.so",
             require     => [ Package["libgrpc-dev"], Package["libgrpc++-dev"] ],
         } ->
         exec { "mavsdk-install":
             user        => "mav",
             timeout     => 0,
-            command     => "/usr/bin/make install >/srv/maverick/var/log/build/mavsdk.install.out 2>&1",
-            cwd         => "/srv/maverick/var/build/mavsdk/build/default",
+            environment => ["LD_LIBRARY_PATH=/srv/maverick/var/build/mavsdk/build/default/third_party/install/lib"],
+            command     => "/usr/bin/cmake --build build/default --target install >/srv/maverick/var/log/build/mavsdk.install.out 2>&1",
+            cwd         => "/srv/maverick/var/build/mavsdk",
             creates     => "/srv/maverick/software/mavsdk/lib/libmavsdk.so",
         } ->
         file { "/srv/maverick/var/build/.install_flag_mavsdk":
