b090166a69cf5e59ee74e1780ed86beaccd4129c
fnoop <fnoopdogg@gmail.com>
2020-01-17 14:18:36 +0000

    Bump collect to 5.10.0

--- manifests/maverick-modules/maverick_analysis/manifests/collect.pp
+++ manifests/maverick-modules/maverick_analysis/manifests/collect.pp
@@ -2,7 +2,7 @@ class maverick_analysis::collect (
     $active = true,
     $install_type = "source",
     $git_source = "https://github.com/collectd/collectd.git",
-    $git_revision = "collectd-5.9.2",
+    $git_revision = "5.10.0",
 ) {
     # Install from source
     if $install_type == "source" {
