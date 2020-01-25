f77326467ad59160f8d59b9d633a6a44c4140762
fnoop <fnoopdogg@gmail.com>
2020-01-25 11:43:11 +0000

    Add docstrings to base::pytho #900

--- manifests/maverick-modules/base/manifests/python.pp
+++ manifests/maverick-modules/base/manifests/python.pp
@@ -1,6 +1,18 @@
+# Base::Python class
+#
+# This class installs/manages Python software.
+# It manages the system python to some degree, but it focuses on
+#   providing a custom optimised python build installed into /srv/maverick/software/python
+# This is currently the latest point release of Python 3.7
+#
+# @example Declaring the class
+#   This class is included from base class and should not be included from elsewhere
+#
+# @param maverick_python Whether to install the custom Maverick Python instance.  This should always be true, unless the OS provides a trusted 3.7 instance.
+# @param python_version The custom version of Python to compmile and install.
 class base::python (
-    $maverick_python = true,
-    $python_version = "v3.7.6"
+    Boolean $maverick_python = true,
+    String $python_version = "v3.7.6",
 ) {
 
     # Install custom python 3.7
