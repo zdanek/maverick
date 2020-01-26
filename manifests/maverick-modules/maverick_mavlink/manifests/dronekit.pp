c59dd4c6d576d5af51a3b1a69bc522b546e3db45
fnoop <fnoopdogg@gmail.com>
2020-01-26 13:52:14 +0000

    Fix dronekit install.. #903

--- manifests/maverick-modules/maverick_mavlink/manifests/dronekit.pp
+++ manifests/maverick-modules/maverick_mavlink/manifests/dronekit.pp
@@ -1,17 +1,51 @@
-class maverick_dev::dronekit (
+class maverick_mavlink::dronekit (
+    $dronekit_la_install = false,
+    $dronekit_la_source = "https://github.com/dronekit/dronekit-la.git",
 ) {
-        
-    file { "/srv/maverick/code/dronekit-apps":
-        ensure      => "directory",
-        owner       => "mav",
-        group       => "mav",
-        mode        => "755",
-    }
     
-    # Install rmackay9 red balloon finder
-    oncevcsrepo { "git-red-balloon-finder":
-        gitsource   => "https://github.com/rmackay9/ardupilot-balloon-finder.git",
-        dest        => "/srv/maverick/code/dronekit-apps/red-balloon-finder",
+    # Install dronekit globally (not in virtualenv) from pip
+    install_python_module { "pip-dronekit-global":
+        pkgname     => "dronekit",
+        ensure      => atleast,
+        version     => "2.9.2",
+        timeout     => 0,
+    }
+
+    if ! ("install_flag_dronekit-la" in $installflags) {
+        # Install dronekit-la (log analyzer)
+        if $dronekit_la_install {
+            oncevcsrepo { "git-dronekit-la":
+                gitsource   => $dronekit_la_source,
+                dest        => "/srv/maverick/var/build/dronekit-la",
+                submodules  => true,
+                owner        => mav,
+                group       => mav,
+            } ->
+            # Compile dronekit-la
+            exec { "compile-dronekit-la":
+                command     => "/usr/bin/make -j${::processorcount} dronekit-la dataflash_logger >/srv/maverick/var/log/build/dronekit-la.build.log 2>&1",
+                creates     => "/srv/maverick/var/build/dronekit-la/dronekit-la",
+                user        => "mav",
+                cwd         => "/srv/maverick/var/build/dronekit-la",
+                timeout     => 0,
+            } ->
+            file { "/srv/maverick/software/dronekit-la":
+                ensure      => directory,
+                owner       => "mav",
+                group       => "mav",
+            } ->
+            exec { "install-dronekit-la":
+                command     => "/bin/cp dronekit-la dataflash_logger README.md /srv/maverick/software/dronekit-la; chmod a+rx /srv/maverick/software/dronekit-la/* >/srv/maverick/var/log/build/dronekit-la.install.log 2>&1",
+                creates     => "/srv/maverick/software/dronekit-la/dronekit-la",
+                user        => "mav",
+                cwd         => "/srv/maverick/var/build/dronekit-la",
+                timeout     => 0,
+            } ->
+            file { "/srv/maverick/var/build/.install_flag_dronekit-la":
+                ensure      => file,
+                owner       => "mav",
+            }
+        }
     }
 
 }
--- manifests/maverick-modules/maverick_mavlink/manifests/init.pp
+++ manifests/maverick-modules/maverick_mavlink/manifests/init.pp
@@ -6,9 +6,6 @@ class maverick_mavlink (
     $mavproxy_install = true,
     $mavproxy_source = "https://github.com/ArduPilot/MAVProxy.git",
     $mavproxy_type = "pip",
-    $dronekit_install = true,
-    $dronekit_la_install = false,
-    $dronekit_la_source = "https://github.com/dronekit/dronekit-la.git",
     $mavcesium = true,
     $mavcesium_install = true,
     $mavcesium_apikey = "Auw42O7s-dxnXl0f0HdmOoIAD3bvbPjFOVKDN9nNKrf1uroCCBxetdPowaQF4XaG",
@@ -32,7 +29,7 @@ class maverick_mavlink (
 
     if $dronekit == true {
         # Install dronekit
-        class { "maverick_dev::dronekit": }
+        class { "maverick_mavlink::dronekit": }
     }
 
     # Create config directory for all mavlink configs
@@ -369,59 +366,6 @@ class maverick_mavlink (
          #   timeout     => 0,
          #   require     => [ Class["maverick_vision::opencv"], Package["libdc1394-22-dev"] ],
         #}
-    }
-    
-    # Install dronekit globally (not in virtualenv) from pip
-    if $dronekit_install {
-        install_python_module { "pip-dronekit-global":
-            pkgname     => "dronekit",
-            ensure      => atleast,
-            version     => "2.9.1",
-            timeout     => 0,
-        }
-    }
-
-    if ! ("install_flag_dronekit-la" in $installflags) {
-        # Install dronekit-la (log analyzer)
-        if $dronekit_la_install {
-            oncevcsrepo { "git-dronekit-la":
-                gitsource   => $dronekit_la_source,
-                dest        => "/srv/maverick/var/build/dronekit-la",
-                submodules  => true,
-                owner        => mav,
-                group       => mav,
-            } ->
-            # Compile dronekit-la
-            exec { "compile-dronekit-la":
-                command     => "/usr/bin/make -j${::processorcount} dronekit-la dataflash_logger >/srv/maverick/var/log/build/dronekit-la.build.log 2>&1",
-                creates     => "/srv/maverick/var/build/dronekit-la/dronekit-la",
-                user        => "mav",
-                cwd         => "/srv/maverick/var/build/dronekit-la",
-                timeout     => 0,
-            } ->
-            file { "/srv/maverick/software/dronekit-la":
-                ensure      => directory,
-                owner       => "mav",
-                group       => "mav",
-            } ->
-            exec { "install-dronekit-la":
-                command     => "/bin/cp dronekit-la dataflash_logger README.md /srv/maverick/software/dronekit-la; chmod a+rx /srv/maverick/software/dronekit-la/* >/srv/maverick/var/log/build/dronekit-la.install.log 2>&1",
-                creates     => "/srv/maverick/software/dronekit-la/dronekit-la",
-                user        => "mav",
-                cwd         => "/srv/maverick/var/build/dronekit-la",
-                timeout     => 0,
-            } ->
-            file { "/srv/maverick/var/build/.install_flag_dronekit-la":
-                ensure      => file,
-                owner       => "mav",
-            }
-        }
-    } else {
-        # This is only here for other manifests to use as a require
-        exec { "install-dronekit-la":
-            command     => "/bin/ls",
-            refreshonly => true,
-        }
     }
     
 }
