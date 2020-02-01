b0d330fb1a699ca8e510d5851ae24b298a14e3c4
fnoop <fnoopdogg@gmail.com>
2020-02-01 12:48:42 +0000

    Turn off extra unwatned services #899

--- manifests/maverick-modules/maverick_security/manifests/disable_services.pp
+++ manifests/maverick-modules/maverick_security/manifests/disable_services.pp
@@ -6,7 +6,24 @@
 #   This class is included from maverick_security class and should not be included from elsewhere
 #
 class maverick_security::disable_services {
-    service { ["atd", "autofs", "haldaemon", "dnsmasq", "portreserve", "named"]:
+    service { [
+        "atd",
+        "autofs",
+        "haldaemon",
+        "dnsmasq",
+        "portreserve",
+        "named",
+    ]:
+        ensure => stopped,
+        enable => false,
+    }
+
+    # These are services that run on the raspberry (maybe others as well)
+    service { [
+        "alsa-state",
+        "exim4",
+        "thd",
+    ]:
         ensure => stopped,
         enable => false,
     }
