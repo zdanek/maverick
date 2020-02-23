afa03ab1c92a2432e2ad2d172930c0cbf491bf3b
fnoop <fnoopdogg@gmail.com>
2020-02-23 18:55:50 +0000

    Move mav_sudopass from users to sudoers manifest #911

--- manifests/maverick-modules/base/manifests/users.pp
+++ manifests/maverick-modules/base/manifests/users.pp
@@ -7,8 +7,6 @@
 #
 # @param mav_password
 #   Hashed password for mav user
-# @param mav_sudopass
-#   If mav sudo should ask for password or not
 # @param root_password
 #   Hashed password for root user
 # @param manage_root_password
@@ -16,7 +14,6 @@
 #
 class base::users (
     String $mav_password = '$6$YuXyoBZR$cR/cNLGZV.Y/nfW6rvK//fjnr84kckI1HM0fhPnJ3MVVlsl7UxaK8vSw.bM4vTlkF4RTbOSAdi36c5d2hJ9Gj1',
-    Boolean $mav_sudopass = false,
     String $root_password = '$6$MIBUXpXc$AA8j.88LvHFBzvVKofKcHnEqvWdv5Cl5D8.O8aB446Mao2X4UkuJ.1VKSr2VcmsbZB7A5ypmmkO0MWGAZr37N.',
     Boolean $manage_root_password = false,
 ) {
@@ -53,23 +50,6 @@ class base::users (
         group		=> "mav",
         mode		=> "644",
     } 
-    if $mav_sudopass == false {
-        file { "/etc/sudoers.d/mav":
-            content 	=> "mav ALL=(ALL) NOPASSWD: ALL",
-            ensure		=> present,
-            owner		=> "root",
-            group		=> "root",
-            mode		=> "644",
-        }
-    } else {
-        file { "/etc/sudoers.d/mav":
-            content 	=> "mav ALL=(ALL) ALL",
-            ensure		=> present,
-            owner		=> "root",
-            group		=> "root",
-            mode		=> "644",
-        }
-    }
     
     if $manage_root_password == true {
         user { "root":
