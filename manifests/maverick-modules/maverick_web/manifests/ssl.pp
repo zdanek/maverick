32a29860c4dfd50f2e358cfbde47a1a2ccb11c07
fnoop <fnoopdogg@gmail.com>
2020-02-25 00:30:58 +0000

    Finish refactor of web ssl setup #921

--- manifests/maverick-modules/maverick_web/manifests/ssl.pp
+++ manifests/maverick-modules/maverick_web/manifests/ssl.pp
@@ -28,20 +28,36 @@ class maverick_web::ssl (
     exec { "create-webssl-key":
         command     => "/usr/bin/openssl genrsa -out ${ssl_location}/${cert_cname}-webssl.key 2048",
         creates     => "${ssl_location}/${cert_cname}-webssl.key",
+        user        => "mav",
+    } ->
+    file { "${ssl_location}/mavweb.key":
+        ensure      => symlink,
+        target      => "${ssl_location}/${cert_cname}-webssl.key",
+        owner       => "mav",
+        group       => "mav",
     } ->
     exec { "create-webssl-csr":
         command     => "/usr/bin/openssl req -new -key ${ssl_location}/${cert_cname}-webssl.key -out ${ssl_location}/${cert_cname}-webssl.csr -subj \"/C=${cert_country}/ST=${cert_state}/L=${cert_locality}/O=${cert_orgname}/OU=${cert_orgunit}/CN=${cert_cname}\"",
         creates     => "${ssl_location}/${cert_cname}-webssl.csr",
+        user        => "mav",
     } ->
     file { "${ssl_location}/${cert_cname}-webssl.ext":
         ensure      => present,
         content     => template("maverick_web/webssl.ext.erb"),
+        owner       => "mav",
+        group       => "mav",
     } ->
     exec { "create-webssl-cert":
         command     => "/usr/bin/openssl x509 -req -passin pass:${ca_passphrase} -in ${ssl_location}/${cert_cname}-webssl.csr -CA /srv/maverick/data/security/ssl/ca/mavCA.pem -CAkey /srv/maverick/data/security/ssl/ca/mavCA.key -CAcreateserial -out ${ssl_location}/${cert_cname}-webssl.crt -days 365 -sha512 -extfile ${ssl_location}/${cert_cname}-webssl.ext",
         creates     => "${ssl_location}/${cert_cname}-webssl.crt",
         require     => Exec["create-ca-rootcert"],
-        notify      => Service["maverick-nginx"]
+        notify      => Service["maverick-nginx"],
+    } ->
+    file { "${ssl_location}/mavweb.crt":
+        ensure      => symlink,
+        target      => "${ssl_location}/${cert_cname}-webssl.crt",
+        owner       => "mav",
+        group       => "mav",
     }
 
 }
