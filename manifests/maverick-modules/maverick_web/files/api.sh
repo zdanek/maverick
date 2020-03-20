09a43d9bd3b4737a5e4644dfcdfc8b41a658b33e
fnoop <fnoopdogg@gmail.com>
2020-03-20 23:39:44 +0000

    Move -api from ~/code to ~/software #926

--- manifests/maverick-modules/maverick_web/files/api.sh
+++ manifests/maverick-modules/maverick_web/files/api.sh
@@ -11,4 +11,4 @@ source /srv/maverick/software/ros/current/setup.bash
 export API_PORT ROS_MASTER_URI
 
 cd /srv/maverick/var/log/web/api/$1
-/srv/maverick/software/python/bin/python3 /srv/maverick/code/maverick-api/maverick-api.py --config-file=/srv/maverick/config/web/maverick-api.$1.conf
+/srv/maverick/software/python/bin/python3 /srv/maverick/software/maverick-api/maverick-api.py --config-file=/srv/maverick/config/web/maverick-api.$1.conf
