42113e17c64379ea5dd399705eee2d2a9435ca2e
fnoop <fnoopdogg@gmail.com>
2020-03-30 09:34:26 +0100

    Order webrtc service after visiond #939

--- manifests/maverick-modules/maverick_web/manifests/janus.pp
+++ manifests/maverick-modules/maverick_web/manifests/janus.pp
@@ -140,7 +140,7 @@ class maverick_web::janus (
         $_enable = false
     }
     file { "/etc/systemd/system/maverick-webrtc.service":
-        content     => template("maverick_web/webrtc.service.erb"),
+        source      => "puppet:///modules/maverick_web/webrtc.service",
         owner       => "root",
         group       => "root",
         mode        => "644",
