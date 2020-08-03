5f04eaeb1ef800dddefaea5640b6c465e879f330
fnoop <fnoopdogg@gmail.com>
2020-08-03 09:56:46 +0100

    Refactor ardu/px4 docs, turn off by default #979

--- manifests/maverick-modules/maverick_web/manifests/maverick_docs.pp
+++ manifests/maverick-modules/maverick_web/manifests/maverick_docs.pp
@@ -16,9 +16,11 @@
 #
 class maverick_web::maverick_docs (
     String $server_hostname = $maverick_web::server_fqdn,
-    Boolean $ardupilot_docs = true,
-    Boolean $px4_docs = true,
-    Boolean $devframe_docs = true,
+    Boolean $ardupilot_docs = false,
+    String $ardupilot_builddir = "/srv/maverick/var/build/ardupilot_wiki",
+    String $ardupilot_installdir = "/srv/maverick/software/ardupilot_wiki",
+    Boolean $px4_docs = false,
+    Boolean $devframe_docs = false,
     String $devframe_docs_gitpath = "https://github.com/goodrobots/devframe.git",
 ) {
 
@@ -100,53 +102,58 @@ class maverick_web::maverick_docs (
     }
 
     if $ardupilot_docs == true {
-        file { "/srv/maverick/var/lib/web/ardupilot_wiki":
-            owner       => "mav",
-            group       => "mav",
-            ensure      => directory,
-        } ->
-        install_python_module { "pip-sphinx":
-            pkgname     => "sphinx",
-            ensure      => present,
-        } ->
-        install_python_module { "pip-sphinx-rtd":
-            pkgname     => "sphinx-rtd-theme",
-            url         => "git+https://github.com/fnoop/sphinx_rtd_theme.git",
-            ensure      => present,
-        } ->
-        install_python_module { "pip-sphinx-youtube":
-            pkgname     => "sphinxcontrib-youtube",
-            url         => "git+https://github.com/sphinx-contrib/youtube.git",
-            ensure      => present,
-        } ->
-        install_python_module { "pip-sphinx-vimeo":
-            pkgname     => "sphinxcontrib.vimeo",
-            url         => "git+https://github.com/fnoop/sphinxcontrib.vimeo.git",
-            ensure      => present,
-        } ->
-        oncevcsrepo { "git-ardupilot_docs":
-            gitsource   => "https://github.com/fnoop/ardupilot_wiki.git",
-            dest        => "/srv/maverick/software/ardupilot_wiki",
-            revision    => "destdir",
-        } ->
-        file { "/srv/maverick/software/ardupilot_wiki/common_conf.py":
-            source      => "puppet:///modules/maverick_web/arduwiki_common_conf.py",
-        } ->
-        file { "/srv/maverick/software/ardupilot_wiki/ardupilot/source/index.rst":
-            source      => "puppet:///modules/maverick_web/arduwiki_index.rst",
-        } ->
-        exec { "ardupilot_wiki_compile":
-            command     => "/srv/maverick/software/python/bin/python3 /srv/maverick/software/ardupilot_wiki/update.py --destdir=/srv/maverick/var/lib/web/ardupilot_wiki",
-            user        => 'mav',
-            timeout     => 0,
-            cwd         => "/srv/maverick/software/ardupilot_wiki",
-            creates     => '/srv/maverick/var/lib/web/ardupilot_wiki/copter/index.html',
-        } ->
+        if ! ("install_flag_ardupilot_wiki" in $installflags) {
+            file { $ardupilot_installdir:
+                owner       => "mav",
+                group       => "mav",
+                ensure      => directory,
+            } ->
+            install_python_module { "pip-sphinx":
+                pkgname     => "sphinx",
+                ensure      => present,
+            } ->
+            install_python_module { "pip-sphinx-rtd":
+                pkgname     => "sphinx-rtd-theme",
+                url         => "git+https://github.com/fnoop/sphinx_rtd_theme.git",
+                ensure      => present,
+            } ->
+            install_python_module { "pip-sphinx-youtube":
+                pkgname     => "sphinxcontrib-youtube",
+                url         => "git+https://github.com/sphinx-contrib/youtube.git",
+                ensure      => present,
+            } ->
+            install_python_module { "pip-sphinx-vimeo":
+                pkgname     => "sphinxcontrib.vimeo",
+                url         => "git+https://github.com/fnoop/sphinxcontrib.vimeo.git",
+                ensure      => present,
+            } ->
+            oncevcsrepo { "git-ardupilot_docs":
+                gitsource   => "https://github.com/fnoop/ardupilot_wiki.git",
+                dest        => $ardupilot_builddir,
+                revision    => "destdir",
+            } ->
+            file { "${ardupilot_builddir}/common_conf.py":
+                source      => "puppet:///modules/maverick_web/arduwiki_common_conf.py",
+            } ->
+            file { "${ardupilot_builddir}/ardupilot/source/index.rst":
+                source      => "puppet:///modules/maverick_web/arduwiki_index.rst",
+            } ->
+            exec { "ardupilot_wiki_compile":
+                command     => "/srv/maverick/software/python/bin/python3 ${ardupilot_builddir}/update.py --destdir=${ardupilot_installdir}",
+                user        => 'mav',
+                timeout     => 0,
+                cwd         => $ardupilot_builddir,
+                creates     => "${ardupilot_installdir}/copter/index.html",
+            } ->
+            file { "/srv/maverick/var/build/.install_flag_ardupilot_wiki":
+                ensure      => present,
+            }
+        }
         nginx::resource::location { "web-ardupilot-wiki":
             ensure          => present,
             ssl             => true,
             location        => "/web/docs/ardupilot",
-            location_alias  => "/srv/maverick/var/lib/web/ardupilot_wiki",
+            location_alias  => $ardupilot_installdir,
             index_files     => ["index.html"],
             server          => $server_hostname,
             require         => [ Class["nginx"], Service["nginx"] ],
